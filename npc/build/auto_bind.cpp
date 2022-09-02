360832a1e45a60c2e37ca04206c29756b1f7bfc4
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-02 22:49:34 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:49:34 up 1 day,  8:14,  4 users,  load average: 0.35, 0.36, 0.32

--- npc/build/auto_bind.cpp
+++ npc/build/auto_bind.cpp
@@ -2,7 +2,5 @@
 #include "Vtop.h"
 
 void nvboard_bind_all_pins(Vtop* top) {
-	nvboard_bind_pin( &top->a, BIND_RATE_SCR, BIND_DIR_IN , 1, SW0);
-	nvboard_bind_pin( &top->b, BIND_RATE_SCR, BIND_DIR_IN , 1, SW1);
-	nvboard_bind_pin( &top->f, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD0);
+	nvboard_bind_pin( &top->led, BIND_RATE_SCR, BIND_DIR_OUT, 16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
 }
