cf2c1b5e048b04722a3475f215f376695c43fbec
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-12 15:46:13 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:46:13 up 2 days,  6:57,  5 users,  load average: 0.61, 0.53, 0.41

--- mytestnpc/csrc/main.cpp
+++ mytestnpc/csrc/main.cpp
@@ -15,11 +15,11 @@ static void single_cycle() {
   dut.clk = 1; dut.eval();
 }
 
-//static void reset(int n) {
-//  dut.rst = 1;
-//  while (n -- > 0) single_cycle();
-//  dut.rst = 0;
-//}
+static void reset(int n) {
+  dut.reset = 1;
+  while (n -- > 0) single_cycle();
+  dut.reset = 0;
+}
 
 int main() {
 
@@ -28,14 +28,14 @@ int main() {
   nvboard_bind_all_pins(&dut);
   nvboard_init(); // initialize NVBOARD nvboard_quit(): 退出NVBoard
 
-//  reset(10);
+  reset(10);
 
   while(1) {
 //在verilator仿真的循环中更新NVBoard各组件的状态
     nvboard_update(); // 更新NVBoard中各组件的状态，每当电路状态发生改变时都需要调用该函数
 //		dut.eval();
-//    single_cycle();
-    dut.eval();
+    single_cycle();
+//    dut.eval();
   }
 
 //退出verilator仿真的循环后，销毁NVBoard的相关资源
