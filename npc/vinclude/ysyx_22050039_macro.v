b6b8faac84eeda4659f7f245c42b9882b038b837
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-22 14:24:59 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-50-generic #56~20.04.1-Ubuntu SMP Tue Sep 27 15:51:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:24:59 up 10:06,  4 users,  load average: 0.46, 0.39, 0.29

--- npc/vinclude/ysyx_22050039_macro.v
+++ npc/vinclude/ysyx_22050039_macro.v
@@ -6,7 +6,7 @@
 		endcase \
 	end
 
-`define INSTPAT(inst, bundle, pattern, imm, type, func, pc_wen) \
+`define INSTPAT(pattern, imm, type, func, pc_wen) \
 	pattern: bundle = {inst[6:0], inst[14:12], \
-		inst[11:7], inst[19:15], inst[24:20], inst[31:25], {{8{inst[31]}}, \
-		inst[31:20]}, 6'b010000, 3'd0, 1'b0}; 
+		inst[11:7], inst[19:15], inst[24:20], inst[31:25], imm, \
+		3'd0, 1'b0}; 
