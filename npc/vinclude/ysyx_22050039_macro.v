06a79d62c06059a0b13d1c5ec33b19b4024f4003
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-22 14:14:17 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-50-generic #56~20.04.1-Ubuntu SMP Tue Sep 27 15:51:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:14:17 up  9:55,  4 users,  load average: 0.15, 0.25, 0.17

--- npc/vinclude/ysyx_22050039_macro.v
+++ npc/vinclude/ysyx_22050039_macro.v
@@ -1,5 +1,7 @@
-`define INSTPAT_START() \
-	always@(*) begin
-	
+`define INSTPAT_START(instruction_wire) \
+	always@(*) begin \
+		casez(instruction_wire)
+
 `define INSTPAT_END() \
+		endcase \
 	end
