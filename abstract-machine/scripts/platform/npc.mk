5e503aa4186ea36933984b691666fdf3d8030d30
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 11:12:48 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-50-generic #56~20.04.1-Ubuntu SMP Tue Sep 27 15:51:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:12:48 up 17:00,  4 users,  load average: 0.30, 0.27, 0.21

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -4,8 +4,8 @@ NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
 run: image
 	@echo "In abm/script/platform/npc.mk"
 	@echo "IMAGE = $(IMAGE)" 	
-	$(MAKE) -C $(NPC_HOME) sim ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME) sim ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin MODE=$(MODE)
 
 gdb: image
-	$(MAKE) -C $(NPC_HOME) gdb ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME) gdb ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin MODE=$(MODE)
 	
