bc6f4ec7a0fb11ce929eb756e44368f7ca43b791
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-29 16:53:33 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:53:33 up  7:20,  3 users,  load average: 0.10, 0.19, 0.13

--- npc/csrc/diff.c
+++ npc/csrc/diff.c
@@ -32,12 +32,8 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   void (*ref_difftest_init)(int) = (void (*)(int))dlsym(handle, "difftest_init");
   assert(ref_difftest_init);
 //
-	printf("cyh-ha 1\n");
   ref_difftest_init(1234);
-	printf("cyh-ha 2\n");
 	ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
-	printf("cyh-ha 3\n");
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-	printf("cyh-ha 4\n");
 }
 
