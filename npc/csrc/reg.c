17044c8c807dd375b565844df55336adfad06adb
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-29 19:43:03 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:43:03 up  8:27,  3 users,  load average: 0.21, 0.35, 0.29

--- npc/csrc/reg.c
+++ npc/csrc/reg.c
@@ -6,7 +6,6 @@ extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
   cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
 
-
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
