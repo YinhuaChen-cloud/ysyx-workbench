adef96e653f22df406ea01025a06b89f5357649e
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 15:42:49 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:42:49 up 13:14,  3 users,  load average: 0.32, 0.24, 0.26

--- npc/vsrc/ysyx_22050039_EXU.v
+++ npc/vsrc/ysyx_22050039_EXU.v
@@ -1,46 +1,68 @@
 `include "ysyx_22050039_all_inst.v"
 
-module ysyx_22050039_EXU #(XLEN = 64) (
-	input clk,
-	input rst,
-	input [`ysyx_22050039_FUNC_LEN-1:0] func,
-	input [XLEN-1:0] src1,
-	input [XLEN-1:0] src2,
-	input [XLEN-1:0] pc,
-	output reg [XLEN-1:0] exec_result,
-	output [XLEN-1:0] dnpc
-);
-
-	import "DPI-C" function void ebreak();
-	import "DPI-C" function void invalid();
-
-	always@(*) begin	
-		exec_result = 0;
-		dnpc = 0;
-		inval = 0;
-		case(func)
-			// Rtype
-			// Itype
-			Addi		: exec_result = src1 + src2;
-			Jalr		: begin exec_result = pc + 4; dnpc = src1 + src2; end
+module ysyx_22050039_EXU #(XLEN = 64)
+                          (input clk,
+                           input rst,
+                           input [`ysyx_22050039_FUNC_LEN-1:0] func,
+                           input [XLEN-1:0] src1,
+                           input [XLEN-1:0] src2,
+                           input [XLEN-1:0] pc,
+                           output reg [XLEN-1:0] exec_result,
+                           output [XLEN-1:0] dnpc);
+  
+  import "DPI-C" function void ebreak();
+  import "DPI-C" function void invalid();
+  
+  always@(*) begin
+    exec_result = 0;
+    dnpc        = 0;
+    inval       = 0;
+    case(func)
+      // Rtype
+      Addw	: exec_result = src1 + src2;
+      Subw	:;
+      Mulw	:;
+      Divw	:;
+      Divuw	:;
+      Sllw	:;
+      Srlw	:;
+      Sraw	:;
+      Remw	:;
+      Remuw	:;
+      Sub	:;
+      Or	:;
+      Add	:;
+      Mul	:;
+      Xor	:;
+      Sll	:;
+      Slt	:;
+      Sltu	:;
+      And	:;
+      Div	:;
+      Divu	:;
+      Rem	:;
+      Remu	:;
+      // Itype
+      Addi	: exec_result = src1 + src2;
+			Jalr	: begin exec_result = pc + 4; dnpc = src1 + src2; end
 			// Stype
-			Sd			: ; // sd empty now
+			Sd	: ; // sd empty now
 			// Btype
 			// Utype
-			Auipc		: exec_result = src1 + pc;
-			Lui			:	exec_result = src1;
+			Auipc	: exec_result = src1 + pc;
+			Lui	:	exec_result   = src1;
 			// Jtype
-			Jal			: begin exec_result = pc + 4; dnpc = pc + src1; end
+			Jal	: begin exec_result = pc + 4; dnpc = pc + src1; end
 			Ebreak	: ebreak();
-			default	: inval = 1; // invalid
-		endcase
-	end
-
-	// invalid is only valid when rst = 0
-	reg inval;
-	always@(posedge clk)
-		if(~rst && inval)
-			invalid();
-		// else do nothing
-			
+			default: inval = 1; // invalid
+    endcase
+  end
+  
+  // invalid is only valid when rst = 0
+  reg inval;
+  always@(posedge clk)
+    if (~rst && inval)
+      invalid();
+      // else do nothing
+  
 endmodule
