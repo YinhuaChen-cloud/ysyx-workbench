0f1e05fa11de9f7d4cae3ad3ff299a2b370fd38c
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-30 13:30:09 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:30:09 up 11:47,  3 users,  load average: 0.19, 0.21, 0.18

--- npc/vinclude/ysyx_22050039_all_inst.v
+++ npc/vinclude/ysyx_22050039_all_inst.v
@@ -4,7 +4,15 @@
 `define ysyx_22050039_FUNC_LEN 8
 
 enum bit[`ysyx_22050039_FUNC_LEN-1:0] {
-	Addi = 0, Jalr, Auipc, Lui, Sd, Jal, Ebreak, Invalid} allinst;
+	Addi = 0, 
+	Jalr, 
+	Auipc, 
+	Lui, 
+	Sd, 
+	Jal, 
+	Ebreak, 
+	Invalid
+	} allinst;
 
 enum bit[5:0] {Rtype = 6'b100000, Itype = 6'b010000, Stype = 6'b001000, Btype = 6'b000100, Utype = 6'b000010, Jtype = 6'b000001, Special = 6'b0} all_inst_types;
 
