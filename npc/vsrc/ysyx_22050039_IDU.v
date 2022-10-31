ea39385ae698f7efa48dcb4979c4b8e88220cda7
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-31 20:14:29 +0800

    > gdb NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:14:29 up 14:40,  4 users,  load average: 0.26, 0.20, 0.19

--- npc/vsrc/ysyx_22050039_IDU.v
+++ npc/vsrc/ysyx_22050039_IDU.v
@@ -141,7 +141,7 @@ module ysyx_22050039_IDU #(XLEN = 64,
 			Special	: ;
 			default : assert(0);
 		endcase
-		$display("inst_type = 0x%x, src1 = 0x%x, src2 = 0x%x", inst_type, src1, src2);
+		$display("inst_type = 0x%x, src1 = 0x%x, src2 = 0x%x, rs1 = %d, rs2 = %d", inst_type, src1, src2, rs1, rs2);
   end
   
   // submodule4 - reg addressing: 5-32 decoder
