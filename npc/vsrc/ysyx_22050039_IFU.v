fc125d52f33c3144eae72a2e0e63be86534088b7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-01 10:46:33 +0800

    > gdb NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:46:33 up 16:49,  4 users,  load average: 0.38, 0.25, 0.15

--- npc/vsrc/ysyx_22050039_IFU.v
+++ npc/vsrc/ysyx_22050039_IFU.v
@@ -13,7 +13,7 @@ module ysyx_22050039_IFU #(XLEN=64) (
 
   ysyx_22050039_Reg #(XLEN, 32'h80000000) pc_reg (clk, rst, next_pc, pc, 1'b1);
 
-  //TODO: the "pc + 1" here may be a problem
+  //TODO: the "pc + 4" here may be a problem
 	assign next_pc = pc_wen ? pc_wdata : pc+4;  
 
 endmodule
