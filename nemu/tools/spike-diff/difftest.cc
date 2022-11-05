f0d94c3704a2e2f214770449d3bbb66a6da1d75a
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-06 02:25:54 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:25:54 up 1 day, 20:29,  4 users,  load average: 0.16, 0.13, 0.10

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -109,6 +109,7 @@ void difftest_regcpy(void* dut, bool direction) {
 
 void difftest_exec(uint64_t n) {
   s->diff_step(n);
+	printf("HEllo, this is yinhua , i am in spike\n");
 }
 
 void difftest_init(int port) {
