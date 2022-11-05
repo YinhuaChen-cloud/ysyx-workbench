0a3905444c61e95d5efae201dd2f5afc4ccea7c4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-05 19:55:41 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:55:41 up 1 day, 13:59,  5 users,  load average: 0.48, 0.27, 0.17

--- npc/csrc/errormessage.c
+++ npc/csrc/errormessage.c
@@ -115,6 +115,8 @@ int is_exit_status_bad() {
   int good = (npc_state.state == NPC_END && npc_state.halt_ret == 0) || 
     (npc_state.state == NPC_QUIT); 
 
+	printf("good = %d\n", good);
+
   return !good;
 }
 
