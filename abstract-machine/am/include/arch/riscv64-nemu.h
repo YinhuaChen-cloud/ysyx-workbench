8a883f7c38ffdfa27bdeee9e9c9015b8d84bf983
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-08 00:26:25 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:26:25 up 2 days, 13:31,  3 users,  load average: 0.03, 0.08, 0.10

--- abstract-machine/am/include/arch/riscv64-nemu.h
+++ abstract-machine/am/include/arch/riscv64-nemu.h
@@ -1,6 +1,15 @@
 #ifndef ARCH_H__
 #define ARCH_H__
 
+//对于上下文, 我们只能将描述上下文的结构体类型名统一成Context, 至于其中的具体内容, 
+//就无法进一步进行抽象了. 这主要是因为不同架构之间上下文信息的差异过大, 
+//比如mips32有32个通用寄存器, 就从这一点来看, mips32和x86的Context注定是无法抽象成完
+//全统一的结构的. 所以在AM中, Context的具体成员也是由不同的架构自己定义的, 比如x86-nemu的
+//Context结构体在abstract-machine/am/include/arch/x86-nemu.h中定义. 因此, 在操
+//作系统中对Context成员的直接引用, 都属于架构相关的行为, 会损坏操作系统的可移植性. 不过大
+//多数情况下, 操作系统并不需要单独访问Context结构中的成员. CTE也提供了一些的接口, 来让
+//操作系统在必要的时候访问它们, 从而保证操作系统的相关代码与架构无关.
+
 struct Context {
   // TODO: fix the order of these members to match trap.S
   uintptr_t mepc, mcause, gpr[32], mstatus;
--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -20,6 +20,10 @@ Context* __am_irq_handle(Context *c) {
 
 extern void __am_asm_trap(void);
 
+//bool cte_init(Context* (*handler)(Event ev, Context *ctx))用于进行CTE相关的初始化操作. 其中它还接受一个来自操作系统的事件处理回调函数的指针, 当发生事件时, CTE将会把事件和相关的上下文作为参数, 来调用这个回调函数, 交由操作系统进行后续处理.
+//cte_init()函数会做两件事情, 第一件就是设置异常入口地址:
+//cte_init()函数做的第二件事是注册一个事件处理回调函数, 这个回调函数由Nanos-lite提供
+//TODO: set exception entry here
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
@@ -34,6 +38,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   return NULL;
 }
 
+//void yield()用于进行自陷操作, 会触发一个编号为EVENT_YIELD事件. 不同的ISA会使用不同的自陷指令来触发自陷操作, 具体实现请RTFSC.
 void yield() {
   asm volatile("li a7, -1; ecall");
 }
