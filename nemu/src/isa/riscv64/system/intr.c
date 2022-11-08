9f73c0c01170390e6c29ad146aa6dedfec6a64d4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-08 19:00:14 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:00:14 up 2 days, 17:59,  4 users,  load average: 0.09, 0.10, 0.09

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -33,7 +33,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 	cpu.mepc = epc;	
 	switch(NO) {
 		case EVENT_YIELD: cpu.mcause = ECALL_FROM_M; // TODO: in the future we should add privilege distinguish
-		default: assert(0);
+		default: Assert(0, "Unsupported event: 0x%lx\n", NO);
 	}
 	
 //	printf("NO = %ld, epc = 0x%lx\n", NO, epc);
