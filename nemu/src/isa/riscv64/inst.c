94717c35feab0e746f2b7112ef597b01c014d919
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-08 19:10:59 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:10:59 up 2 days, 18:10,  4 users,  load average: 0.15, 0.27, 0.19

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -88,9 +88,9 @@ static int decode_exec(Decode *s) {
   INSTPAT_START();
 	// Control status inst
 	// 000000000000 00000 000 00000 1110011
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(EVENT_YIELD, s->snpc)); 
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(EVENT_YIELD, s->pc)); 
 	// csr rs1 010 rd 1110011 
-  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , CR, R(dest) = CSR(csrid); CSR(csrid) |= src1; printf("idx = 0x%lx, csr = 0x%lx\n", csrid, CSR(csrid));); 
+  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , CR, R(dest) = CSR(csrid); CSR(csrid) |= src1;); 
 	// csr rs1 001 rd 1110011
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , CR, R(dest) = CSR(csrid); CSR(csrid) = src1;); 
 	// B
