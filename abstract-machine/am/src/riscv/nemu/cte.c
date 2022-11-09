b99c10311b4e2bd392e41be8534df1125d5f861f
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-10 01:51:33 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:51:33 up  5:34,  3 users,  load average: 0.31, 0.48, 0.44

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -15,7 +15,7 @@ Context* __am_irq_handle(Context *c) {
 	printf("in __am_irq_handle start\n");
 	printf("size of uintptr_t = %ld\n", sizeof(uintptr_t));
   for(int i = 0; i < sizeof(regs)/sizeof(char *); i++) {
-    printf("%s\t0x%lx\t%ld\n", regs[i], c->gpr[i], c->gpr[i]);
+    printf("%s 0x%lx %ld\n", regs[i], c->gpr[i], c->gpr[i]);
   }
 	printf("%s\t0x%lx\t%ld\n", "mcause", c->mcause, c->mcause);
 	printf("%s\t0x%lx\t%ld\n", "mcause", c->mstatus, c->mstatus);
