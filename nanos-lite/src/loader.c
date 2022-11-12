5a7346e2a64803e715553350b271a65627848ea2
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-12 19:12:54 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:12:54 up 23:05,  6 users,  load average: 0.29, 0.33, 0.32

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -42,14 +42,14 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 	extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 	printf("b\n");
-	extern uint8_t ramdisk_start;
+//	extern uint8_t ramdisk_start;
 //	extern uint8_t ramdisk_end;
 
 	printf("a\n");
-	Elf64_Ehdr *elfheader = (Elf64_Ehdr *)(&ramdisk_start); 
+//	Elf64_Ehdr *elfheader = (Elf64_Ehdr *)(&ramdisk_start); 
 	printf("c\n");
 //	printf("machine = %d\n", elfheader->e_machine);
-	printf("machine = %d\n", elfheader->e_machine);
+	printf("machine\n");
 //	assert(*(uint64_t *)elfheader->e_ident == 0x00010102464c457f);	
 //	Elf64_Phdr *program_headers = (Elf64_Phdr *)((uint8_t *)elfheader + elfheader->e_phoff);
 //
