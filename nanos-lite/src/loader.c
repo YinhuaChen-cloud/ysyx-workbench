c5767c534ff641a68c7ab674a64e62fce8df6d70
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-12 11:03:16 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:03:16 up 14:56,  5 users,  load average: 0.05, 0.10, 0.08

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,19 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-#define MEM_SIZE
-#define CONFIG_OS_MSIZE 0x2000000
-static uint8_t osmem[CONFIG_OS_MSIZE];
+// yinhua add this -- start
+#define RAM_READ_BUF_LEN 0x5000
+static uint8_t osmem[RAM_READ_BUF_LEN];
+
+static inline uint8_t  inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
+static inline uint16_t inw(uintptr_t addr) { return *(volatile uint16_t *)addr; }
+static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
+
+static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
+static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
+static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
+static inline void outl64(uintptr_t addr, uint64_t data) { *(volatile uint64_t *)addr = data; }
+// yinhua add this -- end
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
 //	let's do this easy and stupid first
@@ -21,18 +31,19 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 //	2. load program into mem -- the same as above
 //	3. execute the program -- return the entry
 	extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
-	extern uint8_t ramdisk_start;
-	extern uint8_t ramdisk_end;
+	uint64_t *osmem_pointer = (uint64_t *)0x83000000;
 	printf("in loader, before reading ramdisk\n");
-	for(uint16_t *x = (uint16_t *)&ramdisk_start; (uint8_t *)x < &ramdisk_end; x++) {
-		for(int i = 0; i < 8; i++) {
-			printf("%h ", *x);
-		}
-		printf("\n");
+	ramdisk_read(osmem, 0, 0x4c38); // 1
+	for(uint64_t *x = (uint64_t *)osmem; (uint8_t *)x < osmem + 0x4c38; x++, osmem_pointer++) {
+		outl64((uintptr_t)osmem_pointer, *x);
+	}
+	ramdisk_read(osmem, 0x4c38, 0xfe8); // 2
+	for(uint64_t *x = (uint64_t *)osmem; (uint8_t *)x < osmem + 0xfe8; x++, osmem_pointer++) {
+		outl64((uintptr_t)osmem_pointer, *x);
 	}
 	printf("in loader, after reading ramdisk\n");
 //	ramdisk_read(osmem, 0x1000, 0x24d8);
-  return (uintptr_t)osmem; // return entry of the program
+  return 0x83000430; // return entry of the program
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
