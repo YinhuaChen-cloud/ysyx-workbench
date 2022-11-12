6a86aebffb969ba37814ea9e67f100e34000444b
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-12 22:12:24 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:12:24 up 1 day,  2:05,  6 users,  load average: 0.21, 0.40, 0.23

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -51,7 +51,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 	for(Elf64_Phdr *p = program_headers; p < program_headers + elfheader->e_phnum; p++){
 		if(p->p_type != PT_LOAD) 
 			continue;
-		ramdisk_read((uint8_t *)(p->p_vaddr), p->p_offset, p->p_filesz); 
+		ramdisk_read((void *)(p->p_vaddr), p->p_offset, p->p_filesz); 
 //		memset((uint8_t *)(p->p_vaddr) + p->p_filesz, 0, p->p_memsz - p->p_filesz ); // -- zero
 	}
 
