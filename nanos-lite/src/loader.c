1fd7731165d78bd5fe4501a3313c466f50042f01
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-12 15:56:09 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:56:09 up 19:48,  4 users,  load average: 0.16, 0.19, 0.13

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -50,7 +50,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 void naive_uload(PCB *pcb, const char *filename) {
 //	pcb == NULL		filename == NULL
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = aaa %lx", entry);
+  Log("Jump to entry = 0x%lx", entry);
   ((void(*)())entry) (); // execute from location entry
 }
 
