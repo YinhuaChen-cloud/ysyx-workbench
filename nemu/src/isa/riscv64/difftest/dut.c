fd856dc11ec84242f65d13c8cdd9fd2bca4fdbd5
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-12 14:37:35 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:37:35 up 18:30,  4 users,  load average: 0.44, 0.46, 0.27

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -27,12 +27,12 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	for(int i = 0; i < GPR_NR; i++)	{
 		if(cpu.gpr[i] != ref_r->gpr[i]) {
 			theSame = false;
-			printf("------- regs (%s) differs, cpu = 0x%lx, ref = 0x%lx -------\n", regs[i], cpu.gpr[i], ref_r->gpr[i]);
+			printf("------- regs (%s) differs, cpu.%s = 0x%lx, ref.%s = 0x%lx -------\n", regs[i], regs[i], regs[i], cpu.gpr[i], ref_r->gpr[i]);
 		}
 	}
 	if(cpu.pc != ref_r->pc) { 
 		theSame = false;
-		printf("------- pc differs, cpu = 0x%lx, ref = 0x%lx -------\n", cpu.pc, ref_r->pc);
+		printf("------- pc differs, cpu.pc = 0x%lx, ref.pc = 0x%lx -------\n", cpu.pc, ref_r->pc);
 	} 
 	
   return theSame;
