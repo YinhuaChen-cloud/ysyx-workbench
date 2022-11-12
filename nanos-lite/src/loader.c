cc9f6a9c564c9fae84fe9c5efeb59ec68f55ed0f
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-12 11:11:28 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:11:28 up 15:04,  6 users,  load average: 0.24, 0.17, 0.11

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,7 +20,6 @@ static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; 
 static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
 static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
 static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
-static inline void outl64(uintptr_t addr, uint64_t data) { *(volatile uint64_t *)addr = data; }
 // yinhua add this -- end
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
@@ -31,17 +30,17 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 //	2. load program into mem -- the same as above
 //	3. execute the program -- return the entry
 	extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
-	uint64_t *osmem_pointer = (uint64_t *)0x83000000;
+	uint32_t *osmem_pointer = (uint32_t *)0x83000000;
 	printf("in loader, before reading ramdisk\n");
 	ramdisk_read(osmem, 0, 0x4c38); // 1
-	for(uint64_t *x = (uint64_t *)osmem; (uint8_t *)x < osmem + 0x4c38; x++, osmem_pointer++) {
-		outl64((uintptr_t)osmem_pointer, *x);
-		printf("0x%lx\n", *x);
+	for(uint32_t *x = (uint32_t *)osmem; (uint8_t *)x < osmem + 0x4c38; x++, osmem_pointer++) {
+		outl((uintptr_t)osmem_pointer, *x);
+		printf("0x%x\n", *x);
 	}
 	ramdisk_read(osmem, 0x4c38, 0xfe8); // 2
-	for(uint64_t *x = (uint64_t *)osmem; (uint8_t *)x < osmem + 0xfe8; x++, osmem_pointer++) {
-		outl64((uintptr_t)osmem_pointer, *x);
-		printf("0x%lx\n", *x);
+	for(uint32_t *x = (uint32_t *)osmem; (uint8_t *)x < osmem + 0xfe8; x++, osmem_pointer++) {
+		outl((uintptr_t)osmem_pointer, *x);
+		printf("0x%x\n", *x);
 	}
 	printf("in loader, after reading ramdisk\n");
 //	ramdisk_read(osmem, 0x1000, 0x24d8);
