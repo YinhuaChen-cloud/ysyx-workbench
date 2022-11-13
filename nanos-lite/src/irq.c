775a7c006335c9cacc726a7a6de90aec607caca9
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-13 14:17:02 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:17:02 up 1 day,  7:21,  7 users,  load average: 0.25, 0.20, 0.12

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,5 +1,8 @@
 #include <common.h>
 
+#define BITMASK(bits) ((1ull << (bits)) - 1)
+#define BITS(x, hi, lo) (((x) >> (lo)) & BITMASK((hi) - (lo) + 1)) // similar to x[hi:lo] in verilog
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 		case EVENT_YIELD: printf("event yield handled!\n"); c->mepc += 4; break;
@@ -8,6 +11,8 @@ static Context* do_event(Event e, Context* c) {
 			printf("c->mepc = 0x%lx\n", c->mepc);
 			uint32_t tmp_inst = *(uint32_t *)c->mepc;
 			printf("tmp_inst = 0x%lx\n", tmp_inst);
+			int rd  = BITS(tmp_inst, 11, 7);
+			printf("rd = %d\n", rd);
 			printf("event unalign_mem_access handled end!\n"); 
 			c->mepc += 4; 
 			break;
