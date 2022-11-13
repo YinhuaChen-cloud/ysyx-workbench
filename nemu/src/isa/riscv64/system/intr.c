8c77f246171207b877b01519ae246432cdd18493
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-13 13:24:22 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:24:22 up 1 day,  6:28,  7 users,  load average: 0.40, 0.31, 0.29

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -35,10 +35,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
 
-	if(cpu.mstatus & MSTATUS_MIE) {
-		cpu.mstatus |= MSTATUS_MPIE;
-		cpu.mstatus &= ~MSTATUS_MIE;
-	}
+	cpu.mstatus = 0xa00001800;
 
 	cpu.mepc = epc;	
 
