5fb3bb4aa9cf3a22616ed8b91eae99951832af7a
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-13 14:21:55 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:21:55 up 1 day,  7:26,  7 users,  load average: 0.25, 0.21, 0.13

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,10 @@
 #include <common.h>
 
+typedef uint64_t word_t;
+#define SEXT(x, len) ({ struct { int64_t n : len; } __x = { .n = x }; (uint64_t)__x.n; })
 #define BITMASK(bits) ((1ull << (bits)) - 1)
 #define BITS(x, hi, lo) (((x) >> (lo)) & BITMASK((hi) - (lo) + 1)) // similar to x[hi:lo] in verilog
+static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
@@ -12,7 +15,11 @@ static Context* do_event(Event e, Context* c) {
 			uint32_t tmp_inst = *(uint32_t *)c->mepc;
 			printf("tmp_inst = 0x%lx\n", tmp_inst);
 			int rd  = BITS(tmp_inst, 11, 7);
+			int rs1 = BITS(tmp_inst, 19, 15);
+			word_t immediate = immI(tmp_inst);
 			printf("rd = %d\n", rd);
+			printf("rs1 = %d\n", rs1);
+			printf("immediate = 0x%x\n", immediate);
 			printf("event unalign_mem_access handled end!\n"); 
 			c->mepc += 4; 
 			break;
