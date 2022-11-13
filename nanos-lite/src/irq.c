eef1e61bb2a18e3f8d72e46af43281bd87362d27
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-13 12:30:01 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:30:01 up 1 day,  5:54,  6 users,  load average: 0.29, 0.22, 0.19

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 		case EVENT_YIELD: printf("event yield handled!\n"); break;
-		case EVENT_UNALIGN_MEM_ACCESS: printf("event unalign_mem_access handled!\n"); break;
+		case EVENT_UNALIGN_MEM_ACCESS: putstr("event unalign_mem_access handled!\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
