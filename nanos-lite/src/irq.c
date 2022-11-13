8f5c35c0f9e43146e0805d983176a465738e0b0f
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-13 14:31:26 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:31:26 up 1 day,  7:36,  7 users,  load average: 0.53, 0.32, 0.19

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,7 @@
 #include <common.h>
 
 typedef uint64_t word_t;
+typedef word_t vaddr_t;
 #define SEXT(x, len) ({ struct { int64_t n : len; } __x = { .n = x }; (uint64_t)__x.n; })
 #define BITMASK(bits) ((1ull << (bits)) - 1)
 #define BITS(x, hi, lo) (((x) >> (lo)) & BITMASK((hi) - (lo) + 1)) // similar to x[hi:lo] in verilog
@@ -17,6 +18,24 @@ static Context* do_event(Event e, Context* c) {
 			int rd  = BITS(tmp_inst, 11, 7);
 			int rs1 = BITS(tmp_inst, 19, 15);
 			word_t immediate = immI(tmp_inst);
+			int func3 = BITS(tmp_inst, 14,12);
+			vaddr_t src = immediate + c->gpr[rs1];
+			printf("the mem I want to access is 0x%lx\n", src);
+			switch(func3)	{
+				case 3:
+//			imm[11:0] rs1 011 rd 0000011 LD
+					break;
+				default:
+					assert(0);
+//			imm[11:0] rs1 000 rd 0000011 LB
+//			imm[11:0] rs1 100 rd 0000011 LBU
+			}
+
+//			imm[11:0] rs1 001 rd 0000011 LH
+//			imm[11:0] rs1 101 rd 0000011 LHU
+//			imm[11:0] rs1 010 rd 0000011 LW
+//			imm[11:0] rs1 110 rd 0000011 LWU
+
 			printf("rd = %d\n", rd);
 			printf("rs1 = %d\n", rs1);
 			printf("immediate = 0x%x\n", immediate);
