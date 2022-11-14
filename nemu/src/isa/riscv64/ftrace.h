7a30362358a3aa9ab810eff6a315d2e180b3ea47
22050039-Yinhua Chen <12132329@mail.sustech.edu.cn>
2022-11-14 21:08:29 +0800

    add multiple elf ftrace support, and fix a bug in ftrace

--- nemu/src/isa/riscv64/ftrace.h
+++ nemu/src/isa/riscv64/ftrace.h
@@ -17,11 +17,51 @@ int ftrace_indent_space;
 // symtab
 // symtab_size
 // strtab
-Elf64_Sym *symtab;
+Elf64_Sym *symtab = NULL;
 Elf64_Xword symtab_size;
-char *strtab;
+char *strtab = NULL;
+char *ramdisk_elf = NULL;
+Elf64_Sym *ramdisk_symtab = NULL;
+Elf64_Xword ramdisk_symtab_size;
+char *ramdisk_strtab = NULL;
 
 void get_symtab_strtab(){
+	// get ramdisk.img elf
+  FILE *fp = fopen("/home/chenyinhua/sda3/ysyx-workbench/nanos-lite/build/ramdisk.img", "rb");
+	Assert(fp, "ramdisk.img does not exits");
+	if(fp) {
+		fseek(fp, 0, SEEK_END);
+		long size = ftell(fp);
+
+		fseek(fp, 0, SEEK_SET);
+		// TODO: we need to free ramdisk_content after used it, not implemented yet
+		ramdisk_elf = (char *)malloc(size);
+		int ret = fread(ramdisk_elf, size, 1, fp);
+		assert(ret == 1);
+
+		fclose(fp);
+
+		// get strtab
+		Elf64_Ehdr *elfheader = (Elf64_Ehdr *)ramdisk_elf; 
+		//printf("elfheader->e_shoff = %ld\n", elfheader->e_shoff);
+		Elf64_Shdr *section_headers = (Elf64_Shdr *)(ramdisk_elf + elfheader->e_shoff);
+		Elf64_Shdr *strtab_sh = section_headers + elfheader->e_shstrndx - 1;
+		ramdisk_strtab = ramdisk_elf + strtab_sh->sh_addr + strtab_sh->sh_offset;
+//		printf("ramdisk, ramdisk_strtab = %s\n", ramdisk_strtab+1); -- checked
+//		while(1);
+		// get symtab	and symtab_size
+		Elf64_Shdr *p = section_headers;
+		for(int i = 0; i < elfheader->e_shnum; i++){
+			if(p->sh_type == SHT_SYMTAB)
+				break;
+			p++;	
+		}
+
+		ramdisk_symtab_size = p->sh_size;
+		ramdisk_symtab = (Elf64_Sym *)(ramdisk_elf + p->sh_addr + p->sh_offset);
+
+	}
+	
 	// get strtab
 	Elf64_Ehdr *elfheader = (Elf64_Ehdr *)elf_content; 
 	//printf("elfheader->e_shoff = %ld\n", elfheader->e_shoff);
@@ -42,11 +82,7 @@ void get_symtab_strtab(){
 
 // assume the addr must be a FUNC-type symbol in symtab
 char *addrToFunc(Elf64_Addr addr){
-	// 1. compare addr with all entries in symtab
-	//printf("In addrToFunc, addr = 0x%lx\n", addr);
-	//printf("In addrToFunc, symtab = 0x%lx\n", (char *)symtab - elf_content);
-	//printf("In addrToFunc, symtab_size = %lx\n", symtab_size);
-	 	
+	// search addr in nanos-lite elf 	
 	Elf64_Sym *p = symtab;
 	for(; (char *)p < (char *)symtab + symtab_size; p++){
 		//printf("p->st_value = 0x%lx, p->st_size = %ld\n", p->st_value, p->st_size);
@@ -54,10 +90,28 @@ char *addrToFunc(Elf64_Addr addr){
 			break;
 		}
 	}
+
+// 	printf("catch you, addr = 0x%lx\n", addr);
+//	Assert((char *)p < (char *)symtab + symtab_size, "addr = 0x%lx", addr);
+	// search addr in user program elf
+	if((char *)p >= (char *)symtab + symtab_size) {
+		printf("catch you, addr = 0x%lx\n", addr);
+//		assert(0);
+		for(p = ramdisk_symtab; (char *)p < (char *)ramdisk_symtab + ramdisk_symtab_size; p++){
+			//printf("p->st_value = 0x%lx, p->st_size = %ld\n", p->st_value, p->st_size);
+			if(addr >= p->st_value && addr < p->st_value + p->st_size){
+				break;
+			}
+		}
+	}
 	Assert(p != symtab, "p is just symtab");
-	Assert((char *)p < (char *)symtab + symtab_size, "p is out of symtab range, the current pc is 0x%lx", cpu.pc);
+	Assert(p != ramdisk_symtab, "p is just ramdisk_symtab");
+	Assert(((char *)p < (char *)symtab + symtab_size || ((char *)p < (char *)ramdisk_symtab + ramdisk_symtab_size)), "p is out of symtab range, the current pc is 0x%lx", cpu.pc);
 	Assert(ELF64_ST_TYPE(p->st_info) == STT_FUNC, "the entry we found is not FUNC");
-	return strtab + p->st_name;
+	if((char *)p < (char *)symtab + symtab_size)
+		return strtab + p->st_name;
+	else
+		return ramdisk_strtab + p->st_name;
 }
 
 // We assume, if and only if ddest is x1(ra), then it is call
@@ -103,9 +157,10 @@ static void jalr_func(Decode *s, word_t dest, word_t src1, word_t src2) {
 		p += snprintf(p, ftrace_buf + sizeof(ftrace_buf) - p, "0x%lx", s->pc);
 	  drawline(p, ftrace_indent_space);
 		p += ftrace_indent_space;
-		Elf64_Addr func_addr = src1;
+		Elf64_Addr func_addr = (src1 + src2)&(~1);
 		p += snprintf(p, ftrace_buf + sizeof(ftrace_buf) - p, "call [%s@0x%lx]\n", addrToFunc(func_addr), func_addr);
 		fwrite(ftrace_buf, p-ftrace_buf, 1, ftrace_log);
+		fflush(ftrace_log);
 	} else if(isRet(rs1)){
 		// write into ftrace_log: pc_val [indent] ret [func_name]
 		p += snprintf(p, ftrace_buf + sizeof(ftrace_buf) - p, "0x%lx", s->pc);
@@ -113,6 +168,7 @@ static void jalr_func(Decode *s, word_t dest, word_t src1, word_t src2) {
 		p += ftrace_indent_space;
 		p += snprintf(p, ftrace_buf + sizeof(ftrace_buf) - p, "ret [%s]\n", addrToFunc(s->pc)); // we are here
 		fwrite(ftrace_buf, p-ftrace_buf, 1, ftrace_log);
+		fflush(ftrace_log);
 		ftrace_indent_space -= 2;
 	} else {
 		// skip, do nothing
@@ -140,6 +196,7 @@ static void jal_func(Decode *s, word_t dest, word_t src1) {
 		Elf64_Addr func_addr = s->pc + src1;
 		p += snprintf(p, ftrace_buf + sizeof(ftrace_buf) - p, "call [%s@0x%lx]\n", addrToFunc(func_addr), func_addr);
 		fwrite(ftrace_buf, p-ftrace_buf, 1, ftrace_log);
+		fflush(ftrace_log);
 	} else {
 		// skip, do nothing
 //		printf("s->pc = 0x%lx\n", s->pc);
