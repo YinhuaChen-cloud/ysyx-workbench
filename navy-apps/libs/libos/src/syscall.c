65beeedce23650e9419f2eb5c08c6c04f412d6df
22050039-Yinhua Chen <12132329@mail.sustech.edu.cn>
2022-11-14 18:28:37 +0800

    just before implementing multiple-ELF-ftracer

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -48,6 +48,14 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
   register intptr_t _gpr4 asm (GPR4) = a2;
   register intptr_t ret asm (GPRx);
   asm volatile (SYSCALL : "=r" (ret) : "r"(_gpr1), "r"(_gpr2), "r"(_gpr3), "r"(_gpr4));
+
+//	register intptr_t _gpr1 asm ("a7") = type;
+//  register intptr_t _gpr2 asm ("a0") = a0;
+//  register intptr_t _gpr3 asm ("a1") = a1;
+//  register intptr_t _gpr4 asm ("a2") = a2;
+//  register intptr_t ret asm ("a0");
+//  asm volatile ("ecall" : "=r" (ret) : "r"(_gpr1), "r"(_gpr2), "r"(_gpr3), "r"(_gpr4));
+
   return ret;
 }
 
