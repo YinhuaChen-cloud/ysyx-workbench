7d0081b0c10d3dcca08bf8ecbd0984e44a8b1fea
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-15 20:08:53 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:08:53 up 1 day, 18:59,  5 users,  load average: 0.47, 0.26, 0.27

--- nemu/src/isa/riscv64/ftrace.h
+++ nemu/src/isa/riscv64/ftrace.h
@@ -97,11 +97,7 @@ char *addrToFunc(Elf64_Addr addr){
 	if((char *)p >= (char *)symtab + symtab_size) {
 //		assert(0);
 		for(p = ramdisk_symtab; (char *)p < (char *)ramdisk_symtab + ramdisk_symtab_size; p++){
-			if(addr == 0x83004e9c)
-				printf("in ramdisk_symtab, symbol = %s\n", ramdisk_strtab + p->st_name);
 			if(addr >= p->st_value && addr < p->st_value + p->st_size){
-				if(addr == 0x83004e9c)
-					printf("catch you aahahahah\n");
 				break;
 			}
 		}
