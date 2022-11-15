a297fd4b4f2e1a422a86923e79e8801aa4dc5ac4
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-15 13:20:28 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:20:28 up 1 day, 17:31,  3 users,  load average: 0.47, 0.36, 0.36

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -60,7 +60,6 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
 }
 
 void _exit(int status) {
-	printf("in _exit, status = %d\n", status);
   _syscall_(SYS_exit, status, 0, 0);
   while (1);
 }
