0c76e8a37e556a48cf2fd294e39c999d89dcf65a
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-17 01:19:31 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:19:31 up 1 day, 22:41,  4 users,  load average: 0.47, 0.33, 0.30

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,6 +7,12 @@
 //#define GPR4 gpr[0]
 //#define GPRx gpr[0]
 
+//事实上, 我们也可以在Nanos-lite中实现一个简单的strace: Nanos-lite可以得到系统调用的所有信息, 
+//包括名字, 参数和返回值. 这也是为什么我们选择在Nanos-lite中实现strace: 系统调用是携带高层的
+//程序语义的, 但NEMU中只能看到底层的状态机.
+
+#define STRACE
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -14,13 +20,22 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+#ifdef STRACE
+//  switch (a[0]) {
+//		case SYS_yield: 
+//			break;
+//		case SYS_exit: 
+//			break;
+//		case SYS_write:
+//			break;
+//    default: panic("Unhandled syscall ID = %d", a[0]);
+//	}
+#endif
+
   switch (a[0]) {
 		case SYS_yield: 
-			printf("syscall yield handled\n"); 
 			break;
 		case SYS_exit: 
-			printf("syscall exit handled\n"); 
-			printf("a0 = 0x%lx\n", c->GPR2); 
 			halt(c->GPR2);
 			break;
 		case SYS_write:
