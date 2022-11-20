1d826a9e507d96db54a1afd787b3bef495dd5d88
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-20 12:46:58 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:46:58 up 2 days, 20:53,  5 users,  load average: 0.63, 0.44, 0.30

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -11,9 +11,33 @@ int main() {
 	fixedpt A = fixedpt_rconst(1.2);
 	fixedpt B = fixedpt_rconst(2.6);
 
+
+///* Divides two fixedpt numbers, returns the result. */
+//static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
+////	A = a * 2^8		B = b * 2^8       R = (a / b) * 2^8  = (A/B) * 2^8
+//	return (A / B) * FIXEDPT_ONE;
+//}
+//
+//static inline fixedpt fixedpt_abs(fixedpt A) {
+////	A = a * 2^8
+//	return A >= 0 ? A : -A;
+//}
+//
+//static inline fixedpt fixedpt_floor(fixedpt A) {
+//	return A & ~0xff; // TODO: there might be a bug
+//}
+//
+//static inline fixedpt fixedpt_ceil(fixedpt A) {
+//	return (A + 0xff) & ~0xff;
+//}
+
 //static inline fixedpt fixedpt_muli(fixedpt A, int B) {
 	assert(fixedpt_muli(A, 2) == fixedpt_rconst(2.4));
-//#define FIXEDPT_PI	fixedpt_rconst(3.14159265358979323846)
+//static inline fixedpt fixedpt_divi(fixedpt A, int B) {
+	assert(fixedpt_divi(A, 2) == fixedpt_rconst(0.6));
+//static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
+	assert(fixedpt_mul(A, B) == fixedpt_rconst(3.12));
+
 
   printf("PASS!!!\n");
 
