6452eff494335b989610d73be45dc503756c2776
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-22 00:53:41 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:53:41 up 3 days, 10:33,  3 users,  load average: 0.79, 0.72, 0.47

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -10,7 +10,7 @@ int main() {
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
-//	printf("by cyh, w = %d, h = %d\n", w, h);
+	printf("by cyh, in bmp-test main.c w = %d, h = %d\n", w, h);
   NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
