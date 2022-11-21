961a44399f67446e9b98d25a6ecb32c3a402a59c
22050039-Yinhua Chen <12132329@mail.sustech.edu.cn>
2022-11-22 02:38:07 +0800

    pass menu

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -14,6 +14,8 @@ static int memfb_fd = -1;
 
 static int screen_width = -1;
 static int screen_height = -1;	
+static int canvas_width = -1;
+static int canvas_height = -1;	
 
 // 以毫秒为单位返回系统时间
 uint32_t NDL_GetTicks() {
@@ -31,6 +33,8 @@ int NDL_PollEvent(char *buf, int len) {
 	return retval;
 }
 
+// 打开一张(*w) X (*h)的画布
+// 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
 void NDL_OpenCanvas(int *w, int *h) {
 	// get the size of screen -- start
 	char buf[128];
@@ -38,12 +42,21 @@ void NDL_OpenCanvas(int *w, int *h) {
 	read(dispinfo_fd, buf, 128);
 	sscanf(buf, "WIDTH\t: %d\nHEIGHT\t: %d\n", &screen_width, &screen_height);
 	printf("by yinhua, screen_width = %d, screen_height = %d\n", screen_width, screen_height);
+	assert(*w <= screen_width && *h <= screen_height);
+	if(0 == *w && 0 == *h) {
+		*w = screen_width;
+		*h = screen_height;
+	}
+	canvas_width = *w;
+	canvas_height = *h;
 	close(dispinfo_fd);
+	printf("just before getenv\n");
 	// get the size of screen -- end
   if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
     screen_w = *w; screen_h = *h;
+		printf("screen_w = %d, screen_h = %d\n", screen_w, screen_h);
     char buf[64];
     int len = sprintf(buf, "%d %d", screen_w, screen_h);
     // let NWM resize the window and create the frame buffer
@@ -59,9 +72,13 @@ void NDL_OpenCanvas(int *w, int *h) {
   }
 }
 
+// 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
+// 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
 	for(int r = y; r < y + h; r++) {
-		lseek(memfb_fd, (screen_width * r + x) * sizeof(uint32_t), SEEK_SET);
+//		int canvaspos = (screen_width * r + x) * sizeof(uint32_t);
+		int screenpos = (screen_width * (screen_height/2 - canvas_height/2 + r) + (screen_width/2 - canvas_width/2 + x)) * sizeof(uint32_t);
+		lseek(memfb_fd, screenpos, SEEK_SET);
 		write(memfb_fd, pixels + w*(r-y), sizeof(uint32_t) * w);
 	}
 }
