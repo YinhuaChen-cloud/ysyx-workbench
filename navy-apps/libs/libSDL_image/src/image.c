52e912de8b77a492d7260fb04beb809bea43a4e7
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-23 00:47:03 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:47:03 up 3 days, 17:24,  5 users,  load average: 0.02, 0.11, 0.16

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -13,23 +13,23 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-	printf("omg, filename = %s\n", filename);
+	printf("In IMG_Load, omg, filename = %s\n", filename);
 	// 1. 用libc中的文件操作打开文件, 并获取文件大小size
 	FILE *fp = fopen(filename, "r");
 	fseek(fp, 0, SEEK_END);
-	printf("execute fseek successfully\n");
+	printf("In IMG_Load, execute fseek successfully\n");
 	size_t filesize = ftell(fp);
 	// 2. 申请一段大小为size的内存区间buf
 	uint8_t *buf = (uint8_t *)malloc(sizeof(filesize));
-	printf("execute malloc successfully\n");
+	printf("In IMG_Load, execute malloc successfully\n");
 	// 3. 将整个文件读取到buf中
 	fseek(fp, 0, SEEK_SET);
 	fread(buf, filesize, 1, fp);
-	printf("execute fread successfully\n");
+	printf("In IMG_Load, execute fread successfully\n");
 	// 4. 将buf和size作为参数, 调用STBIMG_LoadFromMemory(), 它会返回一个SDL_Surface结构的指针
 	printf("buf = 0x%p\n", buf);
 	SDL_Surface *s = STBIMG_LoadFromMemory(buf, filesize);	
-	printf("execute STBIMGLOAD successfully\n");
+	printf("In IMG_Load, execute STBIMGLOAD successfully\n");
 	// 5. 关闭文件, 释放申请的内存
 	fclose(fp);
 	free(buf);
