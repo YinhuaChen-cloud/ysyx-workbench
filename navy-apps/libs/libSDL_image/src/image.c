39189f5692e48b5de3b1ac9c13019b2fe7bf9e3e
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-22 23:34:35 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:34:35 up 3 days, 16:11,  3 users,  load average: 0.52, 0.40, 0.24

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -4,6 +4,7 @@
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
+#include <stdio.h>
 
 SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(src->type == RW_TYPE_MEM);
@@ -12,7 +13,24 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+	printf("omg, filename = %s\n", filename);
+	// 1. 用libc中的文件操作打开文件, 并获取文件大小size
+	FILE *fp = fopen(filename, "r");
+	fseek(fp, 0, SEEK_END);
+	size_t filesize = ftell(fp);
+	// 2. 申请一段大小为size的内存区间buf
+	uint8_t *buf = (uint8_t *)malloc(sizeof(filesize));
+	// 3. 将整个文件读取到buf中
+	fseek(fp, 0, SEEK_SET);
+	fread(buf, filesize, 1, fp);
+	// 4. 将buf和size作为参数, 调用STBIMG_LoadFromMemory(), 它会返回一个SDL_Surface结构的指针
+	SDL_Surface *s = STBIMG_LoadFromMemory(buf, filesize);	
+	// 5. 关闭文件, 释放申请的内存
+	fclose(fp);
+	free(buf);
+	buf = NULL;
+	// 6. 返回SDL_Surface结构指针
+  return s;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
