111eeb4d4df147ab5d0a7f77586c13e4aa9ed1fd
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 03:59:53 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:59:53 up 4 days,  4:15,  8 users,  load average: 0.37, 0.31, 0.27

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void init_proc() {
 
   // load program here
 	extern void naive_uload(PCB *pcb, const char *filename);
-	naive_uload(NULL, "/bin/pal");
+	naive_uload(NULL, "/bin/bird");
 }
 
 Context* schedule(Context *prev) {
