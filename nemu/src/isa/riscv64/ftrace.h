299e39ddf79a9e9e7ac8506695c056350e928626
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-24 03:24:12 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:24:12 up 4 days,  3:39,  7 users,  load average: 0.13, 0.20, 0.17

--- nemu/src/isa/riscv64/ftrace.h
+++ nemu/src/isa/riscv64/ftrace.h
@@ -55,7 +55,6 @@ void get_symtab_strtab(){
 
 		fseek(fp, file_table[ram_fd].disk_offset, SEEK_SET);
 		long size = file_table[ram_fd].size;
-		printf("In get_symtab_strtab for pal, size = %ld\n", size);
 
 		// TODO: we need to free ramdisk_content after used it, not implemented yet
 		ramdisk_elf = (char *)malloc(size);
@@ -130,7 +129,7 @@ char *addrToFunc(Elf64_Addr addr){
 	Assert(p != symtab, "p is just symtab");
 	Assert(p != ramdisk_symtab, "p is just ramdisk_symtab");
 	Assert(((char *)p < (char *)symtab + symtab_size || ((char *)p < (char *)ramdisk_symtab + ramdisk_symtab_size)), "p is out of symtab range, the current pc is 0x%lx", cpu.pc);
-	Assert(ELF64_ST_TYPE(p->st_info) == STT_FUNC, "[%s:%d] The entry we found is not FUNC", __FILE__, __LINE__);
+	Assert(ELF64_ST_TYPE(p->st_info) == STT_FUNC, "[%s:%d] The entry we found is not FUNC, its st_value = 0x%lx", __FILE__, __LINE__, p->st_value);
 	if(p >= symtab && (char *)p < (char *)symtab + symtab_size)
 		return strtab + p->st_name;
 	else if(p >= ramdisk_symtab && (char *)p < (char *)ramdisk_symtab + ramdisk_symtab_size)
