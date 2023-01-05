7d0dc8b5a50798e8bffd7aa1e5e3cf1cfad2befb
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-05 20:49:49 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:49:49 up 1 day, 11:28,  3 users,  load average: 1.11, 0.71, 0.51

--- npc/playground/src/IFU.scala
+++ npc/playground/src/IFU.scala
@@ -28,7 +28,7 @@ class IFU (xlen: Int) extends Module {
   })
 
   val pc_reg = RegInit("h80000000".U(xlen.W))
-  pc_reg := Mux(io.pc_wen, io.pc_wdata, pc_reg + 4)
+  pc_reg := Mux(io.pc_wen, io.pc_wdata, pc_reg + 4.U)
   io.pc := pc_reg
 
 }
