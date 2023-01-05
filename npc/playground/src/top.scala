5c293c5c208cc724bd4fef3a15a09b53bf03e697
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-05 17:15:47 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:15:47 up 1 day,  7:54,  3 users,  load average: 0.45, 0.51, 0.48

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -5,7 +5,7 @@ import chisel3._
   * Subtracts the smaller from the larger until register y is zero.
   * value in register x is then the GCD
   */
-class GCD extends Module {
+class top extends Module {
   val io = IO(new Bundle {
     val led   = Output(Bool())
   })
