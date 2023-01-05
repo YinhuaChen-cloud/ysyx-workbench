4f4a539131917dc388cb04a0ce01a8a077c67535
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-05 20:11:03 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:11:03 up 1 day, 10:49,  3 users,  load average: 0.40, 0.54, 0.41

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -2,26 +2,12 @@
 
 import chisel3._
 
-/**
-  * Compute GCD using subtraction method.
-  * Subtracts the smaller from the larger until register y is zero.
-  * value in register x is then the GCD
-  */
 class top extends Module {
   val io = IO(new Bundle {
     val led   = Output(Bool())
   })
 
-  val x = RegInit(0.U(1.W))
-  val CNT_MAX = 25000000.U
-  val cnt = RegInit(0.U(32.W))
+  IFU ifu(led)
 
-  when(cnt < CNT_MAX) {
-    cnt := cnt + 1.U
-  }.otherwise {
-    cnt := 0.U
-    x := ~x
-  }
-
-  io.led := x
 }
+
