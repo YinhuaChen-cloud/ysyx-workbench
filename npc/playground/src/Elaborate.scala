9e2f53ea3f67c0db95eb4ee1fbb92c869cd2c7c4
22050039-Yinhua Chen <12132329@mail.sustech.edu.cn>
2023-01-05 17:15:36 +0800

    modify npc to chisel_blinking_led, waiting to test

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -1,7 +1,7 @@
 import circt.stage._
 
 object Elaborate extends App {
-  def top = new GCD()
+  def top = new top()
   val useMFC = true // use MLIR-based firrtl compiler
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   if (useMFC) {
---
+++ npc/playground/src/top.scala
@@ -0,0 +1,17 @@
+import chisel3._
+
+/**
+  * Compute GCD using subtraction method.
+  * Subtracts the smaller from the larger until register y is zero.
+  * value in register x is then the GCD
+  */
+class top extends Module {
+  val io = IO(new Bundle {
+    val led   = Output(Bool())
+  })
+
+  val x = RegInit(0.U(1.W))
+
+  x := ~x
+  io.led := x
+}
