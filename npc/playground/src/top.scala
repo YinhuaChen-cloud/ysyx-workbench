a7145a86ec6b6ad1391b99101e244ca8de802b09
22050039-Yinhua Chen <12132329@mail.sustech.edu.cn>
2023-01-06 10:50:53 +0800

    just before connect to cpp simulation environment

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -1,17 +1,17 @@
+//在Chisel中, when和switch的语义和Verilog的行为建模非常相似, 因此也不建议初学者使用. 相反, 你可以使用MuxOH等库函数来实现选择器的功能, 具体可以查阅Chisel的相关资料.
+
 import chisel3._
 
-/**
-  * Compute GCD using subtraction method.
-  * Subtracts the smaller from the larger until register y is zero.
-  * value in register x is then the GCD
-  */
 class top extends Module {
+  val xlen = 32
   val io = IO(new Bundle {
-    val led   = Output(Bool())
+    val led = Output(Bool())
   })
 
-  val x = RegInit(0.U(1.W))
+  val ifu = Module(new IFU(xlen))
+  ifu.io.pc_wen := 0.U
+  ifu.io.pc_wdata := 1.U
+  io.led := ifu.io.pc(31)
 
-  x := ~x
-  io.led := x
 }
+
