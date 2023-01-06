af19d0bc0365b5a236f9e7d11b02fa94a4bef695
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-06 10:48:39 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:48:39 up 22 min,  2 users,  load average: 0.26, 0.33, 0.28

--- npc/cinclude/expr.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EXPR_H__
-#define __EXPR_H__
-
-void init_regex();
-
-uint64_t expr(char *e, bool *success);
-
-#endif
--- npc/cinclude/sdb.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SDB_H__
-#define __SDB_H__
-
-void sdb_mainloop();
-void init_sdb();
-
-#endif
