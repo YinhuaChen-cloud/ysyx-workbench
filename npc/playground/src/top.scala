96e3252535862e996777b5c9fa5eb5a9b0e0b60f
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-07 15:40:00 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:40:00 up  2:48,  3 users,  load average: 0.41, 0.18, 0.12

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -1,9 +1,4 @@
 //在Chisel中, when和switch的语义和Verilog的行为建模非常相似, 因此也不建议初学者使用. 相反, 你可以使用MuxOH等库函数来实现选择器的功能, 具体可以查阅Chisel的相关资料.
-//
-//存储器中可以放置若干条addi指令的二进制编码(可以利用0号寄存器的特性来编写行为确定的指令)
-//由于目前未实现跳转指令, 因此NPC只能顺序执行, 你可以在NPC执行若干指令之后停止仿真
-//可以通过查看波形, 或者在RTL代码中打印通用寄存器的状态, 来检查addi指令是否被正确执行
-//关于通用寄存器, 你需要思考如何实现0号寄存器的特性; 此外, 为了避免选择Verilog的同学编写出不太合理的行为建模代码, 我们给出如下不完整的代码供大家补充(大家无需改动always代码块中的内容):
 
 import chisel3._
 
@@ -13,10 +8,35 @@ class top (xlen: Int = 64,
     val pc = Output(UInt(xlen.W))
   })
 
-  val ifu = Module(new IFU(xlen))
-  ifu.io.pc_wen := 0.U
-  ifu.io.pc_wdata := 1.U
   io.pc := ifu.io.pc
+
+  // submodule1 IFU
+//	wire pc_wen; // IDU -> IFU
+//	wire [XLEN-1:0] dnpc; // EXU -> IFU
+//	wire [XLEN-1:0] pc; // IFU -> EXU
+  val ifu = Module(new IFU(xlen))
+//  ifu.io.pc_wen := idu.io.pc_wen
+//  ifu.io.pc_wdata := exu.io.dnpc
+//  exu.io.pc := ifu.io.pc
+
+	// submodule2: IDU
+//	wire [XLEN-1:0] src1; // IDU -> EXU
+//	wire [XLEN-1:0] src2; // IDU -> EXU
+//	wire [XLEN-1:0] destI; // IDU -> EXU
+//	wire [`ysyx_22050039_FUNC_LEN-1:0] func; // IDU -> EXU
+//	wire [XLEN-1:0] exec_result; // EXU -> IDU
+//	wire [INST_LEN-1:0] exec_result; // EXU -> IDU
+//	wire [INST_LEN-1:0] inst; // EXU -> IDU
+//  val idu = Module(new IDU(xlen, inst_len))
+//  exu.io.src1 := idu.io.src1
+//  exu.io.src2 := idu.io.src2
+//  exu.io.destI := idu.io.destI
+//  exu.io.func := idu.io.func
+//  idu.io.exec_result := exu.io.exec_result
+//  idu.io.inst := exu.io.result
+//
+//	// submodule3: EXU
+//  val exu = Module(new EXU(xlen, inst_len))
 
 }
 
