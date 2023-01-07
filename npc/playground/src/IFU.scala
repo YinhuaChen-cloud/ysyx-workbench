371092c44ce1d474ccf1930a209cbc8cf134992c
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-08 01:10:55 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-57-generic #63~20.04.1-Ubuntu SMP Wed Nov 30 13:40:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:10:55 up  3:27,  4 users,  load average: 0.06, 0.12, 0.09

--- npc/playground/src/IFU.scala
+++ npc/playground/src/IFU.scala
@@ -1,18 +1,12 @@
 import chisel3._
 import chisel3.util.HasBlackBoxInline
 
-//class IFU (xlen: Int = 64) extends Module {
-class IFU (xlen: Int = 64) extends BlackBox with HasBlackBoxInline {
+class IFU (xlen: Int = 64) extends Module {
   val io = IO(new Bundle {
     val pc_wen = Input(Bool())
     val pc_wdata = Input(UInt(xlen.W))
     val pc = Output(UInt(xlen.W))
   })
-
-  setInline("IFU.v",
-    """import "DPI-C" function void set_pc(input logic [XLEN-1:0] a []);
-      |initial set_pc(pc);  
-    """.stripMargin)
 
   val pc_reg = RegInit("h80000000".U(xlen.W))
   pc_reg := Mux(io.pc_wen, io.pc_wdata, pc_reg + 4.U)
