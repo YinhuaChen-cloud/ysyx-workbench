35ca31568ae27306d6068d41aae9f66cdee7a783
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-09 21:29:56 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-57-generic #63~20.04.1-Ubuntu SMP Wed Nov 30 13:40:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:29:56 up 15:15,  4 users,  load average: 0.05, 0.10, 0.13

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -100,10 +100,10 @@ class IDU (xlen: Int = 64,
   // Only 1 bit of output can be high, and that is the reg to write
   reg_each_wen := MuxLookup(
     rd, "hdeadbeef".U,
-    ArraySeq.unsafeWrapArray(
+    ArraySeq.unsafeWrapArray(Array(
       0.U -> "h0000_0000".U, // $zero is always 0
       1.U -> "h0000_0002".U
-    )
+    ))
   )
 
   reg_total_wen := 1.U 
