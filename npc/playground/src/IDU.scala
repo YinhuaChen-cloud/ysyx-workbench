10c7a76cdd36cc04e54c027e2a3ab1327a2180fa
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-11 21:34:35 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-57-generic #63~20.04.1-Ubuntu SMP Wed Nov 30 13:40:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:34:35 up 1 day, 15:30,  6 users,  load average: 0.82, 0.46, 0.29

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -94,7 +94,7 @@ class IDU (xlen: Int = 64,
     decoded_output := MuxCase(0.U,
       ArraySeq.unsafeWrapArray(Array(
         RV64Instr.ADDI(io.inst) -> Cat("h1234".U(16.W), "h5678".U(16.W)),
-        RV64Instr.EBREAK(io.inst) -> "h1234_5678".U 
+        RV64Instr.EBREAK(io.inst) -> "deadbeef".U 
     // `ysyx_22050039_INSTPAT(32'b?????????????????000?????0010011, {{8{inst[31]}}, inst[31:20]}, Itype, Addi, `ysyx_22050039_NO_WPC, `ysyx_22050039_WREG)
     // `ysyx_22050039_INSTPAT(32'b00000000000100000000000001110011, 20'b0, Special, Ebreak, `ysyx_22050039_NO_WPC, `ysyx_22050039_NO_WREG)
     //`define ysyx_22050039_INSTPAT(pattern, imm, type, func, pc_wen, reg_wen) \
