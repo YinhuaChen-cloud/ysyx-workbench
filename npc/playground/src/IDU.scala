a2cea52072527a4d0d59b5453a0ced4ae83dde31
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-11 21:31:17 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-57-generic #63~20.04.1-Ubuntu SMP Wed Nov 30 13:40:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:31:17 up 1 day, 15:27,  6 users,  load average: 0.28, 0.16, 0.18

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -103,9 +103,9 @@ class IDU (xlen: Int = 64,
 //		type, func, pc_wen, reg_wen}; 
      ))
    )
-   printf("1 = 0x%x\n", Cat(Seq("h1234".U, "h5678".U)))
-   printf("2 = 0x%x\n", Cat("h1234".U(16.W), "h5678".U(16.W)))
-   printf("3 = 0x%x\n", "h1234_5678".U)
+  printf("1 = 0x%x\n", Cat(Seq("h1234".U(16.W), "h5678".U(16.W))))
+  printf("2 = 0x%x\n", Cat("h1234".U(16.W), "h5678".U(16.W)))
+  printf("3 = 0x%x\n", "h1234_5678".U)
 
   // submodule4 - reg addressing: 5-32 decoder
   // Only 1 bit of output can be high, and that is the reg to write
