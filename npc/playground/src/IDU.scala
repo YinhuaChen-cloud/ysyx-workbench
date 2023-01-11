9f209a0fee9b98d0ecb8b0a8d622dcdd3dc95ec7
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-11 20:25:00 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-57-generic #63~20.04.1-Ubuntu SMP Wed Nov 30 13:40:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:25:00 up 1 day, 14:20,  7 users,  load average: 0.25, 0.28, 0.16

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -92,7 +92,7 @@ class IDU (xlen: Int = 64,
    // The core of DecodeUnit
    val decoded_output = Wire(UInt(32.W))
    decoded_output := MuxLookup(
-     io.inst, 0.UInt(32.W),
+     io.inst, 0.U(32.W),
      Array(
        RV64Instr.ADDI -> "h1234_5678".U,
        RV64Instr.EBREAK -> "h1234_5678".U 
