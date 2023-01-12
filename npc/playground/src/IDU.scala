d3071b6a7dfcb99af6756eb1a5ed29638d105d70
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-12 15:13:48 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-57-generic #63~20.04.1-Ubuntu SMP Wed Nov 30 13:40:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:13:48 up 1 day, 22:25,  7 users,  load average: 0.53, 0.36, 0.26

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -27,6 +27,8 @@ object RV64CPUMacros {
   }
 }
 
+import RV64CPUMacros
+
 // addi
 // auipc
 // jal
@@ -74,7 +76,7 @@ class IDU (xlen: Int = 64,
 
   class Decoded_output extends Bundle {
     val imm = Wire(UInt(20.W))
-    val InstType = Wire(RV64InstrType())
+    val InstType = Wire(InstType())
     val exuop = Wire(UInt(macros.func_len.W)) // TODO: need to connect with io
     val pc_wen = Wire(Bool()) // TODO: need to connect with io
     val reg_total_wen = Wire(Bool()) // TODO: need to connect with io
