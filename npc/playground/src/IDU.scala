5ae9de1e82cd2edf56665d50e6e9c3015c258d50
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-12 17:11:59 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-57-generic #63~20.04.1-Ubuntu SMP Wed Nov 30 13:40:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:11:59 up 2 days, 24 min,  7 users,  load average: 0.36, 0.40, 0.34

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -113,7 +113,7 @@ class IDU (xlen: Int = 64,
       Btype.asUInt -> reg_stack(rs1),
       Utype.asUInt -> Cat(Fill(xlen-32, unpacked.imm(19)), unpacked.imm, Fill(xlen-32-20, 0.U)), // TODO: assume only 32-bit and 64-bit CPU are supported
       Jtype.asUInt -> Cat(Fill(xlen-20-1, unpacked.imm(19)), unpacked.imm, 0.U), // TODO: assume only 32-bit and 64-bit CPU are supported
-      // Special -> do nothing, return 0
+      Special.asUInt -> 0.U,
     ))
   )
    
