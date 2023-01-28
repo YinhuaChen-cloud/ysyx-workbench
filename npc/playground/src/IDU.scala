2a320e5f81997f7f063011bc2cddac4889778197
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-29 01:11:04 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:11:04 up 14:29,  3 users,  load average: 0.68, 0.44, 0.29

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -8,7 +8,7 @@ import chisel3.util._
 //import chisel3.experimental.ChiselEnum
 //import scala.collection.immutable.ArraySeq
 import Macros._
-import Macros.RV64ExuOp._
+//import Macros.RV64ExuOp._
 import Macros.RV64Inst._
 import Macros.Constants._
 
