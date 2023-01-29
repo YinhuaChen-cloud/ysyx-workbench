1572dca7aa6352ba07f8b9a70d0c1b0509507c20
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-29 17:19:31 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:19:31 up 18:17,  3 users,  load average: 0.38, 0.28, 0.21

--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -2,7 +2,12 @@ import chisel3._
 import chisel3.util._
 
 class top extends Module {
+  val io = IO(new Bundle{
+    val pc      = Output(UInt(32.W))
+  })
+
   val ifu = Module(new IFU)
   val exu = Module(new EXU)
   ifu.io <> exu.io
+  io.pc := ifu.io.pc
 }
