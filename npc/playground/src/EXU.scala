367b669b86c45f6c80a5d0e1b7e11aa8ad2debf0
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-29 18:17:11 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:17:11 up 19:14,  3 users,  load average: 0.34, 0.17, 0.12

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -88,10 +88,6 @@ class EXU (xlen: Int = 64,
   val jr_target  = Wire(UInt(32.W))
 //  val exception_target = Wire(UInt(32.W))
 
-  pc_plus4   := (io.pc + 4.asUInt(xlen.W))
-  jmp_target := io.pc + imm_j_sext
-  jr_target  := rs1_data + imm_i_sext 
-
   io.pc_next := MuxCase(pc_plus4, Array(
                (io.pc_sel === PC_4)   -> pc_plus4,
 //               (io.ctl.pc_sel === PC_BR)  -> br_target,
@@ -99,6 +95,11 @@ class EXU (xlen: Int = 64,
                (io.pc_sel === PC_JR)  -> jr_target,
 //               (io.ctl.pc_sel === PC_EXC) -> exception_target
                ))
+
+  pc_plus4   := (io.pc + 4.asUInt(xlen.W))
+  jmp_target := io.pc + imm_j_sext
+  jr_target  := rs1_data + imm_i_sext 
+
 //
 
 //  // submodule4 - wb_data
