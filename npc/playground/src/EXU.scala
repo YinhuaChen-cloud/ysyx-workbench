1053058eece1c1bf6a215666d2e2d2983134b6b8
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-29 14:15:18 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:15:18 up 16:43,  3 users,  load average: 0.38, 0.40, 0.38

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -1,23 +1,14 @@
 import chisel3._
 import chisel3.util._
 import chisel3.stage.ChiselStage
-//import scala.collection.immutable.ArraySeq
 
 import Macros._
 import Macros.Constants._
-//import Macros.RV64ExuOp._
 
 class EXU (xlen: Int = 64, 
   inst_len: Int = 32,
   nr_reg: Int = 32) extends Module {
   val io = IO(new Bundle {
-//    val exuop = Input(RV64ExuOp())
-//    val src1 = Input(UInt(xlen.W))
-//    val src2 = Input(UInt(xlen.W))
-//    val destI = Input(UInt(xlen.W))
-//    val exec_result = Output(UInt(xlen.W))
-//    val isEbreak = Output(Bool())
-//    val pc = Input(UInt(xlen.W))
     val inst = Input(UInt(inst_len.W))
 
     val pc_sel    = Input(UInt(BR_N.getWidth.W))
@@ -26,7 +17,6 @@ class EXU (xlen: Int = 64,
     val alu_op    = Input(UInt(ALU_X.getWidth.W))
     val reg_wen   = Input(Bool())
 
-//    val pc_next   = Output(UInt(xlen.W))
     val pc = Output(UInt(xlen.W))
   })
 
@@ -35,8 +25,6 @@ class EXU (xlen: Int = 64,
   val pc_reg = RegInit("h8000_0000".U(32.W))
   pc_reg := pc_next
   io.pc := pc_reg
-//  pc_reg := Mux(io.pc_wen, io.pc_wdata, pc_reg + 4.U)
-//  io.pc := pc_reg
 
 
   // submodule1 - register file
@@ -49,12 +37,6 @@ class EXU (xlen: Int = 64,
   // 1-3. register file
   val regfile = RegInit(VecInit(Seq.fill(nr_reg)(0.U(xlen.W))))
   regfile(rd_addr) := Mux((rd_addr =/= 0.U && io.reg_wen), wb_data, regfile(rd_addr))
-//  val reg_each_wen = Wire(UInt(nr_reg.W)) // TODO: not drive yet
-//  val reg_total_wen = Wire(Bool()) // TODO: not drive yet
-//  regfile(0) := 0.U // $zero/x0 is always 0 TODO: what will happen to pending wire?
-//  for(i <- 1 to nr_reg-1) {
-//    regfile(i) := Mux(reg_total_wen & reg_each_wen(i), io.exec_result, regfile(i)) 
-//  }
 
   // submodule2 - ALU
   val rs1_data = Mux((rs1_addr =/= 0.U), regfile(rs1_addr), 0.asUInt(xlen.W))
@@ -123,84 +105,6 @@ class EXU (xlen: Int = 64,
   jr_target  := rs1_data + imm_i_sext 
 
   // submodule4 - comparison --- for BR mostly
-  
-  
-  
-  
-  
-  
-
-////  class ADDER (width: Int = 64) extends Module {
-////    val io = IO(new Bundle{
-////      val input1 = Input(UInt(width.W))
-////      val input2 = Input(UInt(width.W))
-////      val sum = Output(UInt(width.W))
-////    }) 
-////    
-////    io.sum := io.input1 + io.input2
-////
-////  }
-//
-//  // The core of ExecuteUnit
-//  val tmpsrc1 = Wire(UInt(xlen.W)) 
-//  val tmpsrc2 = Wire(UInt(xlen.W)) 
-//
-//  tmpsrc1 := MuxLookup(
-//    io.exuop.asUInt, 0.U,
-//    ArraySeq.unsafeWrapArray(Array(
-//      Addi.asUInt -> io.src1,
-//      Auipc.asUInt -> io.src1,
-//      Jal.asUInt -> io.pc,
-//      Jalr.asUInt -> io.pc,
-//    ))
-//  )
-//
-//  tmpsrc2 := MuxLookup(
-//    io.exuop.asUInt, 0.U,
-//    ArraySeq.unsafeWrapArray(Array(
-//      Addi.asUInt -> io.src2,
-//      Auipc.asUInt -> io.pc,
-//      Jal.asUInt -> 4.U,
-//      Jalr.asUInt -> 4.U,
-//    ))
-//  )
-//  
-//  val adder1 = Module(new ADDER(xlen))
-//  io.exec_result := adder1.io.sum
-//  adder1.io.input1 := tmpsrc1
-//  adder1.io.input2 := tmpsrc2
-//
-////      SD -> Cat(Fill(xlen-32, unpacked.imm(19)), unpacked.imm, Fill(xlen-32-20, 0.U)),
-////      EBREAK -> Cat(Fill(xlen-20-1, unpacked.imm(19)), unpacked.imm, 0.U),
-////
-//
-//  val dnpc_src1 = Wire(UInt(xlen.W)) 
-//  val dnpc_src2 = Wire(UInt(xlen.W)) 
-//  
-//  dnpc_src1 := MuxLookup(
-//    io.exuop.asUInt, 0.U,
-//    ArraySeq.unsafeWrapArray(Array(
-//      Jal.asUInt -> io.pc,
-//      Jalr.asUInt -> io.src1,
-//    ))
-//  )
-//
-//  dnpc_src2 := MuxLookup(
-//    io.exuop.asUInt, 0.U,
-//    ArraySeq.unsafeWrapArray(Array(
-//      Jal.asUInt -> io.src1,
-//      Jalr.asUInt -> io.src2,
-//    ))
-//  )
-//  val adder2 = Module(new ADDER(xlen))
-//  io.dnpc := adder2.io.sum
-//  adder2.io.input1 := dnpc_src1
-//  adder2.io.input2 := dnpc_src2
-//
-////      EBREAK -> Cat(Fill(xlen-20-1, unpacked.imm(19)), unpacked.imm, 0.U),
-//  io.isEbreak := Mux((io.exuop === Ebreak), true.B, false.B)
-//
-////      SD -> Cat(Fill(xlen-32, unpacked.imm(19)), unpacked.imm, Fill(xlen-32-20, 0.U)),
   
 
 }
