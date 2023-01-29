0eccdba2cee3476f4ba9fab40425140fe1a633df
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-29 15:33:17 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:33:17 up 17:06,  3 users,  load average: 0.51, 0.33, 0.29

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -104,6 +104,7 @@ class EXU (xlen: Int = 64,
   jr_target  := rs1_data + imm_i_sext 
   printf("rs1_data = 0x%x, imm_i_sext = 0x%x\n", rs1_data, imm_i_sext)
   printf("ra, regfile(1) = 0x%x\n", regfile(1))
+  printf("rd_addr = 0x%x\n", rd_addr)
 
   // submodule4 - comparison --- for BR mostly
   
