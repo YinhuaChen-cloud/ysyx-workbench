bae96f00fcc85b8df81a02978b39580505e81239
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-29 13:52:34 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:52:34 up 16:20,  3 users,  load average: 0.30, 0.51, 0.46

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -100,13 +100,15 @@ class EXU (xlen: Int = 64,
   val jr_target  = Wire(UInt(32.W))
 //  val exception_target = Wire(UInt(32.W))
 
-  io.pc_next := MuxCase(pc_plus4, Array(
-               (io.pc_sel === PC_4)   -> pc_plus4,
-//               (io.ctl.pc_sel === PC_BR)  -> br_target,
-               (io.pc_sel === PC_J )  -> jmp_target,
-               (io.pc_sel === PC_JR)  -> jr_target,
-//               (io.ctl.pc_sel === PC_EXC) -> exception_target
-               ))
+//  io.pc_next := MuxCase(pc_plus4, Array(
+//               (io.pc_sel === PC_4)   -> pc_plus4,
+////               (io.ctl.pc_sel === PC_BR)  -> br_target,
+//               (io.pc_sel === PC_J )  -> jmp_target,
+//               (io.pc_sel === PC_JR)  -> jr_target,
+////               (io.ctl.pc_sel === PC_EXC) -> exception_target
+//               ))
+  io.pc_next := io.pc + 4.U(xlen.W)
+
   pc_plus4   := (io.pc + 4.asUInt(xlen.W))
   jmp_target := io.pc + imm_j_sext
   jr_target  := rs1_data + imm_i_sext 
