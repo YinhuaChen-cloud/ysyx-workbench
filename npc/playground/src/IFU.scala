4d672785a8df76e35052e4ed34e0f339fa35bda5
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-30 16:39:28 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:39:28 up 1 day, 17:30,  3 users,  load average: 0.24, 0.28, 0.29

--- npc/playground/src/IFU.scala
+++ npc/playground/src/IFU.scala
@@ -2,11 +2,13 @@ import chisel3._
 import chisel3.util._
 import Conf._
 
+class IFU_to_EXU (implicit val conf: Configuration) extends Bundle() {
+  val pc_next = Input(UInt(conf.pc_len.W))
+  val pc      = Output(UInt(conf.pc_len.W))
+}
+
 class IFU (implicit val conf: Configuration) extends Module {
-  val io = IO(new Bundle{
-    val pc_next = Input(UInt(conf.pc_len.W))
-    val pc      = Output(UInt(conf.pc_len.W))
-  })
+  val io = IO(new IFU_to_EXU())
 
   val pc_reg = RegInit(conf.START_ADDR)
   pc_reg := io.pc_next
