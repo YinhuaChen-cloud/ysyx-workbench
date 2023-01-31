a52a91861b54d6945811871639457913cf210fd6
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-31 19:51:20 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:51:20 up  4:20,  3 users,  load average: 0.30, 0.35, 0.25

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -32,7 +32,7 @@ class EXU (implicit val conf: Configuration) extends Module {
   val io_regfile_aux = dontTouch(Wire(Vec(conf.nr_reg * conf.xlen, Bool())))
   val whatever = VecInit(Seq(true.B, true.B, true.B))
   io_regfile_aux := DontCare
-  io_regfile_aux.slice(2, 0).zip(regfile(0)(2, 0).asBools).foreach {case (a,b) => a := b}
+  io_regfile_aux.slice(2, 0) := regfile(0)(2, 0)
 //  io_regfile_aux(0) := regfile(0)(0)
   io.regfile := DontCare 
 
