31cc8452657bc0bc3afa964f4daf9e8e48249fb6
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-31 16:56:52 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:56:52 up  1:25,  3 users,  load average: 0.08, 0.15, 0.12

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -26,7 +26,8 @@ class EXU (implicit val conf: Configuration) extends Module {
   // 1-3. register file
   val regfile = RegInit(VecInit(Seq.fill(conf.nr_reg)(0.U(conf.xlen.W))))
   regfile(rd_addr) := Mux((rd_addr =/= 0.U && io.idu_to_exu.reg_wen), wb_data, regfile(rd_addr))
-  io.regfile := regfile.flatten
+  val mytest := regfile.flatten
+//  io.regfile := regfile.flatten
 
   // submodule2 - ALU
   val rs1_data = Mux((rs1_addr =/= 0.U), regfile(rs1_addr), 0.asUInt(conf.xlen.W))
