d94941e84941475db605407108601c36cc54dca9
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-31 20:36:50 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:36:50 up 22 min,  3 users,  load average: 0.34, 0.35, 0.30

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -117,7 +117,7 @@ static long load_img() {
 }
 
 static void reset(int n) {
-	printf("In reset, pc = 0x%lx\n", pc);
+	printf("In reset, pc = 0x%p\n", pc);
 	printf("In reset, *pc = 0x%lx\n", *pc);
   top->reset = 1;
   while (n -- > 0) single_cycle();
