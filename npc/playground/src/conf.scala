cd6e0ac6e83f30a925669dd81bb0675b1cb6993c
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-01 09:30:48 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:30:48 up  1:49,  3 users,  load average: 0.13, 0.17, 0.16

--- npc/playground/src/conf.scala
+++ npc/playground/src/conf.scala
@@ -6,7 +6,7 @@ case class Configuration() {
   val xlen = 64
   val inst_len = 32
   val nr_reg = 32
-  val pc_len = 32
+  val pc_len = xlen
   val START_ADDR = "h8000_0000".U(32.W)
 //   val nxprbits = log2Ceil(nxpr)
 //   val rvc = false
