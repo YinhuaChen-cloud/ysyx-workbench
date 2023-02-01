c3dd8dfcdbe9f98abddebf12172c600e81dd619c
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-01 09:42:03 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:42:03 up  2:00,  3 users,  load average: 0.29, 0.23, 0.19

--- npc/playground/src/DPIC.scala
+++ npc/playground/src/DPIC.scala
@@ -22,7 +22,8 @@ with HasExtModuleInline {
               |           input io_rst,
               |           input [PC_LEN-1:0] io_pc,
               |           input io_isEbreak,
-              |           input io_inv_inst);
+              |           input io_inv_inst,
+              |           input [NR_REG * XLEN - 1:0] io_regfile);
               |
               |  import "DPI-C" function void set_pc(input logic [PC_LEN-1:0] a []);
               |  initial set_pc(io_pc);  
@@ -44,7 +45,6 @@ with HasExtModuleInline {
 
 }
 
-//              |           input [NR_REG * XLEN - 1:0] io_regfile);
 //              |  import "DPI-C" function void set_gpr_ptr(input logic [XLEN-1:0] a []);
 //              |  initial set_gpr_ptr(io_regfile);
 
