fcd3444e07c5cabf1eccae106e20a5c93691273a
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-05 11:55:58 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:55:58 up 18:37,  4 users,  load average: 0.48, 0.37, 0.30

--- npc/csrc/paddr.c
+++ npc/csrc/paddr.c
@@ -62,6 +62,8 @@ extern "C" void pmem_read(long long raddr, long long *rdata) {
 	*rdata = *(long long *)cpu_to_sim(raddr & ~0x7ull);
 //	 mtrace -> NOTE: we need to judge whether raddr is a inst or a data -> human assistance	
 #ifdef CONFIG_MTRACE
+	printf("raddr = 0x%lx, *pc = 0x%lx, cpu.pc = 0x%lx, pc_just_exec = 0x%lx\n", 
+			raddr, *pc, cpu.pc, pc_just_exec);
 	if(raddr == *pc) // filter out inst reading
 		return;
 	snprintf(mtrace_buf, MTRACE_BUF_LEN, "pc:0x%8lx %5s addr:0x%8llx data:0x%8llx\n", cpu.pc, "Read", raddr, *rdata); 
