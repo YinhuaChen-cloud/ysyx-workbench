22b77d263d0a7104e04804b8d8429959e7c04b0e
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-05 15:03:04 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:03:04 up 21:45,  6 users,  load average: 0.47, 0.51, 0.36

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -133,7 +133,7 @@ class EXU (implicit val conf: Configuration) extends Module {
   val mem_in_sel_sext = Wire(UInt(conf.xlen.W)) 
   val mem_in_result = Wire(UInt(conf.xlen.W)) 
   mem_in_sel_sext := MuxCase(mem_in_sel, Array( // by default, mem_msk is -1.U(64.W)
-    (io.idu_to_exu.mem_msk === "hffff_ffff".U) -> Cat(Fill(32, mem_in_sel(31)), mem_in_sel(31, 0)),
+    (io.idu_to_exu.mem_msk === "hffff_ffff".U) -> Cat(Fill(conf.xlen - 32, mem_in_sel(31)), mem_in_sel(31, 0)),
     ))
   mem_in_result := Mux(io.idu_to_exu.sign_op, mem_in_sel_sext, mem_in_sel)
 
