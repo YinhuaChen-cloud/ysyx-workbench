30d47e843b3379638c94f2adb6e17a7b67d16a21
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-07 02:14:24 +0800

    > sim RTL ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:14:24 up 1 day, 16:35,  5 users,  load average: 0.44, 0.49, 0.54

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -106,7 +106,7 @@ class EXU (implicit val conf: Configuration) extends Module {
       (io.idu_to_exu.alu_op === ALU_MUX)    -> (alu_op1 * alu_op2).asUInt(),
       (io.idu_to_exu.alu_op === ALU_SLTU)   -> (alu_op1 < alu_op2).asUInt(),
       (io.idu_to_exu.alu_op === ALU_SLL)    -> (alu_op1 << alu_shamt).asUInt(),
-      (io.idu_to_exu.alu_op === ALU_AND)    -> (alu_op1 & alu_shamt).asUInt(),
+      (io.idu_to_exu.alu_op === ALU_AND)    -> (alu_op1 & alu_op2).asUInt(),
     )
   )
 
