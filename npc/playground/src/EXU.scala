3cda57c526b57d237a679d2b31fc538aa333358a
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-07 02:38:17 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:38:17 up 1 day, 16:58,  5 users,  load average: 0.71, 0.74, 0.79

--- npc/playground/src/EXU.scala
+++ npc/playground/src/EXU.scala
@@ -109,6 +109,7 @@ class EXU (implicit val conf: Configuration) extends Module {
       (io.idu_to_exu.alu_op === ALU_SLL)    -> (alu_op1 << alu_shamt).asUInt(),
       (io.idu_to_exu.alu_op === ALU_AND)    -> (alu_op1 & alu_op2).asUInt(),
       (io.idu_to_exu.alu_op === ALU_OR)     -> (alu_op1 | alu_op2).asUInt(),
+      (io.idu_to_exu.alu_op === ALU_XOR)    -> (alu_op1 ^ alu_op2).asUInt(),
     )
   )
 
