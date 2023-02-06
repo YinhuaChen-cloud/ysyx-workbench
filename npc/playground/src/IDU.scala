9f9ba7272634e755273e9b73777a382c966de8e1
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-07 03:05:38 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:05:38 up 1 day, 17:26,  5 users,  load average: 0.86, 1.20, 1.05

--- npc/playground/src/IDU.scala
+++ npc/playground/src/IDU.scala
@@ -62,6 +62,7 @@ class IDU (implicit val conf: Configuration) extends Module {
       SLLIW     -> List(Y, BR_N  , OP1_RS1, OP2_IMI, ALU_SLL , WB_ALU, WREG_1, WMEM_0, MEM_MSK_X , ALU_MSK_W, SIGN_N),
       SRLI      -> List(Y, BR_N  , OP1_RS1, OP2_IMI, ALU_SRL , WB_ALU, WREG_1, WMEM_0, MEM_MSK_X , ALU_MSK_X, SIGN_N),
       SRAI      -> List(Y, BR_N  , OP1_RS1, OP2_IMI, ALU_SRA , WB_ALU, WREG_1, WMEM_0, MEM_MSK_X , ALU_MSK_X, SIGN_N),
+      SRAIW     -> List(Y, BR_N  , OP1_RS1, OP2_IMI, ALU_SRA , WB_ALU, WREG_1, WMEM_0, MEM_MSK_X , ALU_MSK_W, SIGN_N),
       ANDI      -> List(Y, BR_N  , OP1_RS1, OP2_IMI, ALU_AND , WB_ALU, WREG_1, WMEM_0, MEM_MSK_X , ALU_MSK_X, SIGN_Y),
       XORI      -> List(Y, BR_N  , OP1_RS1, OP2_IMI, ALU_XOR , WB_ALU, WREG_1, WMEM_0, MEM_MSK_X , ALU_MSK_X, SIGN_Y),
       JALR      -> List(Y, BR_JR , OP1_RS1, OP2_IMI, ALU_X   , WB_PC4, WREG_1, WMEM_0, MEM_MSK_X , ALU_MSK_X, SIGN_N),
