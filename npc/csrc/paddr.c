d8cdb67de0407ccb4630f6b8b8fabc653daad584
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-07 23:52:48 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-58-generic #64~20.04.1-Ubuntu SMP Fri Jan 6 16:42:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:52:48 up 2 days,  8:26,  2 users,  load average: 0.31, 0.23, 0.17

--- npc/csrc/paddr.c
+++ npc/csrc/paddr.c
@@ -72,12 +72,12 @@ extern "C" void pmem_read(long long raddr, long long *rdata) {
 }
 
 extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
-//	// peripheral
-//	if(waddr == CONFIG_SERIAL_PORT) {
-//		printf("%c", (char)(wdata & 0xff));
-//		difftest_skip_ref();
-//		return;
-//	}
+	// peripheral
+	if(waddr == CONFIG_SERIAL_PORT) {
+		printf("%c", (char)(wdata & 0xff));
+		difftest_skip_ref();
+		return;
+	}
   // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
