0e19bf8d310df6e514891138a57dff9c9037d84e
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-15 15:29:48 +0800

    > compile NEMU ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:29:48 up  9:17,  6 users,  load average: 7.18, 6.40, 3.96

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -35,46 +35,45 @@
 //// yinhua add this -- end
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-////	1. read program from ramdisk -- invoke ramdisk_read()
-////	2. load program into mem -- the same as above
-////	3. execute the program -- return the entry
-//	printf("in loader, before reading ramdisk, filename = %s\n", filename);
-//
-//	int fd = fs_open(filename, 0, 0);
-//
-//	Elf_Ehdr elfheader_entity;
-//	Elf_Ehdr *elfheader = &elfheader_entity; 
-//
-//	fs_read(fd, (void *)elfheader, sizeof(Elf_Ehdr));
-//
-//	assert(elfheader->e_machine == EXPECT_TYPE);	
-//	assert(*(uint64_t *)elfheader->e_ident == 0x00010102464c457f);	
-//
-//	Elf_Phdr program_headers_entity;
-//	Elf_Phdr *program_headers = &program_headers_entity;
-//	fs_lseek(fd, elfheader->e_phoff, SEEK_SET);
-//	fs_read(fd, (void *)program_headers, sizeof(Elf_Phdr));
-//
-//	for(int i = 1; i < elfheader->e_phnum; i++){
-//		if(program_headers->p_type != PT_LOAD) {
-//			fs_lseek(fd, elfheader->e_phoff + i*elfheader->e_phentsize, SEEK_SET);
-//			fs_read(fd, (void *)program_headers, sizeof(Elf_Phdr));
-//			continue;
-//		}
-//
-//		fs_lseek(fd, program_headers->p_offset, SEEK_SET);
-//		fs_read(fd, (void *)(program_headers->p_vaddr), program_headers->p_filesz);
-//		memset((uint8_t *)(program_headers->p_vaddr) + program_headers->p_filesz, 0, program_headers->p_memsz - program_headers->p_filesz ); // -- zero
-//
-//		fs_lseek(fd, elfheader->e_phoff + i*elfheader->e_phentsize, SEEK_SET);
-//		fs_read(fd, (void *)program_headers, sizeof(Elf_Phdr));
-//	}
-//
-//	printf("in loader, after reading ramdisk, filename = %s\n", filename);
-//
-//	fs_close(fd);
-//  return elfheader->e_entry; // return entry of the program
-	return 0;
+//	1. read program from ramdisk -- invoke ramdisk_read()
+//	2. load program into mem -- the same as above
+//	3. execute the program -- return the entry
+	printf("in loader, before reading ramdisk, filename = %s\n", filename);
+
+	int fd = fs_open(filename, 0, 0);
+
+	Elf_Ehdr elfheader_entity;
+	Elf_Ehdr *elfheader = &elfheader_entity; 
+
+	fs_read(fd, (void *)elfheader, sizeof(Elf_Ehdr));
+
+	assert(elfheader->e_machine == EXPECT_TYPE);	
+	assert(*(uint64_t *)elfheader->e_ident == 0x00010102464c457f);	
+
+	Elf_Phdr program_headers_entity;
+	Elf_Phdr *program_headers = &program_headers_entity;
+	fs_lseek(fd, elfheader->e_phoff, SEEK_SET);
+	fs_read(fd, (void *)program_headers, sizeof(Elf_Phdr));
+
+	for(int i = 1; i < elfheader->e_phnum; i++){
+		if(program_headers->p_type != PT_LOAD) {
+			fs_lseek(fd, elfheader->e_phoff + i*elfheader->e_phentsize, SEEK_SET);
+			fs_read(fd, (void *)program_headers, sizeof(Elf_Phdr));
+			continue;
+		}
+
+		fs_lseek(fd, program_headers->p_offset, SEEK_SET);
+		fs_read(fd, (void *)(program_headers->p_vaddr), program_headers->p_filesz);
+		memset((uint8_t *)(program_headers->p_vaddr) + program_headers->p_filesz, 0, program_headers->p_memsz - program_headers->p_filesz ); // -- zero
+
+		fs_lseek(fd, elfheader->e_phoff + i*elfheader->e_phentsize, SEEK_SET);
+		fs_read(fd, (void *)program_headers, sizeof(Elf_Phdr));
+	}
+
+	printf("in loader, after reading ramdisk, filename = %s\n", filename);
+
+	fs_close(fd);
+  return elfheader->e_entry; // return entry of the program
 
 }
 
