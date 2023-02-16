e0fc6fb2247e760260880ebc8a55ec1bc772bae5
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-16 11:34:30 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:34:30 up  2:37,  5 users,  load average: 5.56, 5.61, 5.84

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -1,8 +1,9 @@
+-include $(NPC_HOME)/include/config/auto.conf
+-include $(NPC_HOME)/include/config/auto.conf.cmd
 
-ifeq ($(MYDIFF), TRUE)
+# ifeq ($(CONFIG_DIFFTEST), y)
 	# NPCFLAGS += --diff=${NEMU_HOME}/build/riscv64-nemu-interpreter-so
-	NPCFLAGS += --diff=/home/chenyinhua/sda3/ysyx-workbench/nemu/tools/spike-diff/build/riscv64-spike-so
-endif
+# endif
 
 NPCFLAGS += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
 
@@ -11,16 +12,13 @@ NPCFLAGS += --mtrace=$(IMAGE)-mtrace.txt
 run: image
 	@echo "In abm/script/platform/npc.mk"
 	@echo "IMAGE = $(IMAGE)" 	
-	$(MAKE) -C $(NPC_HOME) sim ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin MODE=$(MODE) WALL=$(WALL) \
-		MYDIFF=$(MYDIFF)
+	$(MAKE) -C $(NPC_HOME) sim ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin WALL=$(WALL)
 
 gdb: image
-	$(MAKE) -C $(NPC_HOME) gdb ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin MODE=$(MODE) WALL=$(WALL) \
-		MYDIFF=$(MYDIFF)
+	$(MAKE) -C $(NPC_HOME) gdb ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin WALL=$(WALL)
 	
 sdb: image
 	@echo "In npc.mk, I am sdb=========="
 	@echo "In npc.mk, NPCFLAGS = $(NPCFLAGS), IMAGE = $(IMAGE)"
-	$(MAKE) -C $(NPC_HOME) sdb ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin MODE=$(MODE) WALL=$(WALL) \
-		MYDIFF=$(MYDIFF)
+	$(MAKE) -C $(NPC_HOME) sdb ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin WALL=$(WALL)
 	
