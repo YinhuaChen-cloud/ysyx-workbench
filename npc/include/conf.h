0252d35d959be790c01438dad801194e1749f2db
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-16 11:22:41 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:22:41 up  2:25,  5 users,  load average: 5.76, 6.12, 6.00

--- npc/include/conf.h
+++ npc/include/conf.h
@@ -2,7 +2,7 @@
 #define __CONF_H__
 
 // configuration about npc
-#define CONFIG_MSIZE 0x2000000 // 64MB
+//#define CONFIG_MSIZE 0x2000000 //
 #define CONFIG_MBASE 0x80000000
 #define CONFIG_PC_RESET_OFFSET 0x0
 
