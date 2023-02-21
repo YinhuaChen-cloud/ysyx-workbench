ea1762ca03e65e5b61300c7f7480e694789fd78d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-21 20:12:22 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:12:22 up 1 day,  6:17,  5 users,  load average: 0.27, 0.97, 1.23

--- npc/playground/src/MDU.scala
+++ npc/playground/src/MDU.scala
@@ -18,7 +18,7 @@ class MDU (implicit val conf: Configuration) extends Module {
   io.result := MuxCase(
     0.U, Array(
 //      (io.alu_op === ALU_MUX)    -> (alu_op1 * alu_op2).asUInt(),
-      (io.alu_op === ALU_DIV)    -> (alu_op1 / alu_op2).asUInt(),
+      (io.alu_op === ALU_DIV)    -> (io.alu_op1 / io.alu_op2).asUInt(),
 //      (io.alu_op === ALU_DIV && io.idu_to_exu.alu_msk_type =/= ALU_MSK_W)    -> (alu_op1.asSInt / alu_op2.asSInt).asUInt(),
 //      (io.alu_op === ALU_DIV && io.idu_to_exu.alu_msk_type === ALU_MSK_W)    -> (alu_op1(31, 0).asSInt / alu_op2(31, 0).asSInt).asUInt(),
 //      (io.alu_op === ALU_REM)    -> (alu_op1 % alu_op2).asUInt(),
