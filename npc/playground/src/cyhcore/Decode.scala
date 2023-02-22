fbb47fcd98cdc92b41248cd4b0101ca709191dde
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-22 16:56:57 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:56:57 up 1 day, 14:36,  4 users,  load average: 0.58, 0.25, 0.20

--- npc/playground/src/cyhcore/Decode.scala
+++ npc/playground/src/cyhcore/Decode.scala
@@ -1,11 +1,16 @@
-// import chisel3._
-// import chisel3.util._
+package cyhcore
 
-// object Instructions {
-//   def NOP = 0x00000013.U
-//   def DecodeTable = RVIInstr.table ++ NutCoreTrap.table ++
-//     (if (HasMExtension) RVMInstr.table else Nil) ++
-//     // Priviledged.table ++
-//     // RVZicsrInstr.table
-// }
+import chisel3._
+import chisel3.util._
+
+import Macros._
+import Macros.Constants._
+
+object Instructions {
+  def NOP = 0x00000013.U
+  val DecodeDefault = List(N, BR_N , OP1_X  , OP2_X  , ALU_X   , WB_X  , WREG_0, WMEM_0, MEM_MSK_X , ALU_MSK_X, SIGN_X)
+  def DecodeTable = RVIInstr.table ++ RVMInstr.table ++
+    Priviledged.table ++
+    // RVZicsrInstr.table ++ RVZifenceiInstr.table
+}
 
