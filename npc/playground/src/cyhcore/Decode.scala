8298948546f49581d2b54c96f83834c256ce1271
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-22 15:56:14 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:56:14 up 1 day, 13:36,  4 users,  load average: 0.15, 0.18, 0.14

--- npc/playground/src/cyhcore/Decode.scala
+++ npc/playground/src/cyhcore/Decode.scala
@@ -1,12 +1,11 @@
-import chisel3._
-import chisel3.util._
+// import chisel3._
+// import chisel3.util._
 
-object Instructions {
-  def NOP = 0x00000013.U
-  def DecodeTable = RVIInstr.table ++ NutCoreTrap.table ++
-    (if (HasMExtension) RVMInstr.table else Nil) ++
-    // Priviledged.table ++
-    // RVZicsrInstr.table
-}
-
+// object Instructions {
+//   def NOP = 0x00000013.U
+//   def DecodeTable = RVIInstr.table ++ NutCoreTrap.table ++
+//     (if (HasMExtension) RVMInstr.table else Nil) ++
+//     // Priviledged.table ++
+//     // RVZicsrInstr.table
+// }
 
