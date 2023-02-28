414a01febbf5564460b31f175fb963a8c300a23a
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-28 10:50:14 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:50:14 up 18:14,  1 user,  load average: 0.04, 0.43, 0.42

--- npc/playground/src/cyhcore/frontend/IFU.scala
+++ npc/playground/src/cyhcore/frontend/IFU.scala
@@ -9,6 +9,10 @@ class IFU_to_EXU (implicit val conf: Configuration) extends Bundle() {
   val pc      = Output(UInt(conf.pc_len.W))
 }
 
+// 1. 用RTL编写一个AXI-Lite的SRAM模块, 地址位宽为32bit, 数据位宽为64bit
+// 2. 收到读请求后, 通过DPI-C调用pmem_read(), 并延迟一周期返回读出的数据
+    // IFU每次取指都要等待一个周期, 才能取到指令交给IDU
+
 class IFU (implicit val conf: Configuration) extends Module {
   val io = IO(new IFU_to_EXU())
 
