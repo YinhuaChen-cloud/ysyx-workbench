7026d82bf0f5af655b7f5cc272e898272255c1a4
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-03 15:36:09 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-60-generic #66~20.04.1-Ubuntu SMP Wed Jan 25 09:41:30 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:36:09 up 15:36,  1 user,  load average: 0.78, 0.40, 0.39

--- npc/playground/src/utils/DPIC.scala
+++ npc/playground/src/utils/DPIC.scala
@@ -4,26 +4,74 @@ import chisel3._
 import chisel3.util._
 import chisel3.experimental._
 import Conf._
+import cyhcore.HasCyhCoreParameter
 
-class DPIC (implicit val conf: Configuration) 
-extends ExtModule(Map("XLEN" -> conf.xlen, "NR_REG" -> conf.nr_reg, "PC_LEN" -> conf.pc_len, "INST_LEN" -> conf.inst_len)) 
-with HasExtModuleInline {
+// class DPIC (implicit val conf: Configuration) 
+// extends ExtModule(Map("XLEN" -> conf.xlen, "NR_REG" -> conf.nr_reg, "PC_LEN" -> conf.pc_len, "INST_LEN" -> conf.inst_len)) 
+// with HasExtModuleInline {
+//   val io = IO(new Bundle {
+//     val clk = Input(Clock())
+//     val rst = Input(Bool())
+//     val isEbreak = Input(Bool())
+//     val inv_inst  = Input(Bool())
+//     val regfile = Input(UInt((conf.nr_reg * conf.xlen).W))
+//   })
+
+//   setInline("DPIC.v",
+//             s"""
+//               |module DPIC #(XLEN=${conf.xlen}, NR_REG=${conf.nr_reg}, PC_LEN=${conf.pc_len}, INST_LEN=${conf.inst_len}) (
+//               |           input io_clk,
+//               |           input io_rst,
+//               |           input io_isEbreak,
+//               |           input io_inv_inst,
+//               |           input [NR_REG * XLEN - 1:0] io_regfile);
+//               |
+//               |  // only cpp simulation environment can execute ebreak()
+//               |  import "DPI-C" function void ebreak();
+//               |  always@(*) begin
+//               |    if(io_isEbreak)
+//               |      ebreak();   
+//               |  end
+//               |
+//               |  // used to report invalid inst error
+//               |  import "DPI-C" function void invalid();
+//               |    always@(posedge io_clk)
+//               |      if (~io_rst && io_inv_inst)
+//               |        invalid();
+//               |
+//               |  // expose regfile for difftest
+//               |  import "DPI-C" function void set_gpr_ptr(input logic [XLEN-1:0] a []);
+//               |  wire [XLEN-1:0] regs [NR_REG-1:0];
+//               |  genvar i;
+//               |  generate
+//               |    for(i = 0; i < NR_REG; i = i+1) begin
+//               |      assign regs[i] = io_regfile[(i+1)*XLEN - 1 : i*XLEN]; 
+//               |    end
+//               |  endgenerate
+//               |  initial set_gpr_ptr(regs);
+//               |
+//               |endmodule
+//             """.stripMargin)
+
+// }
+
+class DPIC extends BlackBox with HasBlackBoxInline with HasCyhCoreParameter {
   val io = IO(new Bundle {
     val clk = Input(Clock())
     val rst = Input(Bool())
     val isEbreak = Input(Bool())
     val inv_inst  = Input(Bool())
-    val regfile = Input(UInt((conf.nr_reg * conf.xlen).W))
+    val regfile = Input(UInt((NR_GPRS * XLEN).W))
   })
 
   setInline("DPIC.v",
             s"""
-              |module DPIC #(XLEN=${conf.xlen}, NR_REG=${conf.nr_reg}, PC_LEN=${conf.pc_len}, INST_LEN=${conf.inst_len}) (
+              |module DPIC (
               |           input io_clk,
               |           input io_rst,
               |           input io_isEbreak,
               |           input io_inv_inst,
-              |           input [NR_REG * XLEN - 1:0] io_regfile);
+              |           input [${NR_GPRS} * ${XLEN} - 1:0] io_regfile);
               |
               |  // only cpp simulation environment can execute ebreak()
               |  import "DPI-C" function void ebreak();
@@ -39,12 +87,12 @@ with HasExtModuleInline {
               |        invalid();
               |
               |  // expose regfile for difftest
-              |  import "DPI-C" function void set_gpr_ptr(input logic [XLEN-1:0] a []);
-              |  wire [XLEN-1:0] regs [NR_REG-1:0];
+              |  import "DPI-C" function void set_gpr_ptr(input logic [${XLEN}-1:0] a []);
+              |  wire [${XLEN}-1:0] regs [${NR_GPRS}-1:0];
               |  genvar i;
               |  generate
-              |    for(i = 0; i < NR_REG; i = i+1) begin
-              |      assign regs[i] = io_regfile[(i+1)*XLEN - 1 : i*XLEN]; 
+              |    for(i = 0; i < ${NR_GPRS}; i = i+1) begin
+              |      assign regs[i] = io_regfile[(i+1)*${XLEN} - 1 : i*${XLEN}]; 
               |    end
               |  endgenerate
               |  initial set_gpr_ptr(regs);
