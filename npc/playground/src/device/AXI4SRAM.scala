622bf6ecee4a2589b67594672f548160feadfba1
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-06 23:54:36 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:54:36 up 18:05,  1 user,  load average: 1.63, 1.23, 1.13

--- npc/playground/src/device/AXI4SRAM.scala
+++ npc/playground/src/device/AXI4SRAM.scala
@@ -4,6 +4,37 @@ import chisel3._
 import chisel3.util._
 import cyhcore.HasCyhCoreParameter
 
+              // |  // Define the state enumeration
+              // |  // typedef enum logic [1:0] {
+              // |  //   IDLE,
+              // |  //   BUSY
+              // |  // } state_t;
+              // |  
+              // |  // Define the state variable
+              // |  // reg state;
+              // |
+              // |  reg [${XLEN}-1:0]	inst_aux;
+              // |
+              // |  // Define the state transition logic
+              // |  always @(posedge clk) begin
+              // |    if (rst) begin
+              // |      pc_ready <= 1'b0;
+              // |      inst_valid <= 1'b0;
+              // |      inst_aux = '0; 
+              // |    end 
+              // |    else begin
+              // |      if(pc_valid) begin
+              // |        pc_ready <= 1'b1;
+              // |        inst_valid <= 1'b1;
+              // |        pmem_read(pc, inst_aux); 
+              // |      end
+              // |      else begin
+              // |        pc_ready <= 1'b0;
+              // |        // do nothing, 
+              // |      end
+              // |    end
+              // |  end
+
 // class AXI4SRAMnew extends AXI4SlaveModule {
 //   val idle :: reading_data :: Nil = Enum(2)
 //   val state  = RegInit(idle)
@@ -27,6 +58,14 @@ import cyhcore.HasCyhCoreParameter
 // 2. 收到读请求后, 通过DPI-C调用pmem_read(), 并延迟一周期返回读出的数据
     // IFU每次取指都要等待一个周期, 才能取到指令交给IDU
 // ================================================
+
+// 3. 当从机（slave）接收到读请求（ar-valid）且处于空闲状态则产生返还给主机（master）读准备(ar-ready)信号
+// 表明可以进行读取操作，这时就完成了一次读地址的握手，
+// 4. 从机（slave）开始准备需要返回的数据（r-data）、读返回请求（r-valid）、(读完成（r-last），读完成不需要，AXILite没有) 
+// 5. 主机此时也跳变到下一个读数据状态，产生并发出可以读取返回数据的状态读准备（r-ready），当主机的r-valid & r-ready
+// 完成握手，主机得到需要的数据（r-data），
+// 6. 当主机接收到读完成（r-last）则完成了一次读事务同时状态跳变到空闲状态，并且产生读完成信号（ready），
+// 将指令数据（inst）返还给取指模块（IF）。
 
 class AXI4SRAMnew extends BlackBox with HasBlackBoxInline with HasCyhCoreParameter {
   val io = IO(new Bundle {
@@ -63,52 +102,6 @@ class AXI4SRAMnew extends BlackBox with HasBlackBoxInline with HasCyhCoreParamet
               |
               |  // for mem_r
               |  import "DPI-C" function void pmem_read(input longint raddr, output longint rdata);
-// 3. 当从机（slave）接收到读请求（ar-valid）且处于空闲状态则产生返还给主机（master）读准备(ar-ready)信号
-// 表明可以进行读取操作，这时就完成了一次读地址的握手，
-// 4. 从机（slave）开始准备需要返回的数据（r-data）、读返回请求（r-valid）、(读完成（r-last），读完成不需要，AXILite没有) 
-// 5. 主机此时也跳变到下一个读数据状态，产生并发出可以读取返回数据的状态读准备（r-ready），当主机的r-valid & r-ready
-// 完成握手，主机得到需要的数据（r-data），
-// 6. 当主机接收到读完成（r-last）则完成了一次读事务同时状态跳变到空闲状态，并且产生读完成信号（ready），
-// 将指令数据（inst）返还给取指模块（IF）。
-              |  // Define the state enumeration
-              |  // typedef enum logic [1:0] {
-              |  //   IDLE,
-              |  //   BUSY
-              |  // } state_t;
-              |  
-              |  // Define the state variable
-              |  // reg state;
-
-    // val clk = Input(Clock())
-    // val rst = Input(Bool())
-
-    // val pc_valid = Input(Bool())
-    // val pc = Input(UInt(XLEN.W))
-
-    // val inst_ready = Input(Bool())
-
-              |
-              |  reg [${XLEN}-1:0]	inst_aux;
-              |
-              |  // Define the state transition logic
-              |  always @(posedge clk) begin
-              |    if (rst) begin
-              |      pc_ready <= 1'b0;
-              |      inst_valid <= 1'b0;
-              |      inst_aux = '0; 
-              |    end 
-              |    else begin
-              |      if(pc_valid) begin
-              |        pc_ready <= 1'b1;
-              |        inst_valid <= 1'b1;
-              |        pmem_read(pc, inst_aux); 
-              |      end
-              |      else begin
-              |        pc_ready <= 1'b0;
-              |        // do nothing, 
-              |      end
-              |    end
-              |  end
               |  // inst selection	
               |  always@(*) 
               |    case(pc[2:0])
