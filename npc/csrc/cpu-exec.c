d086f3b1fcc4f5f7610ba4336c5ba61a5b2323b1
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-08 02:36:50 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:36:50 up 1 day,  9:51,  1 user,  load average: 0.04, 0.21, 0.23

--- npc/csrc/cpu-exec.c
+++ npc/csrc/cpu-exec.c
@@ -15,25 +15,25 @@ int first_flag = 0;
 #endif
 
 void single_cycle() {
-#ifdef CONFIG_WAVEFORM
-  if(!first_flag) { // 打印还没有 eval() 时的波形
-    // tfp->dump(contextp->time()); // 此时还没有调用过 context->timeInc()， 实际上这里的 dump 是无效的
-    first_flag = 1;
-  }
-#endif
-
-  top->clock = 0; top->eval();
-#ifdef CONFIG_WAVEFORM
-  contextp->timeInc(1); // necessary for wave gen
-  tfp->dump(contextp->time());
-#endif
-  void dealWithExit();
-  dealWithExit();
-//   top->clock = 1; top->eval();
 // #ifdef CONFIG_WAVEFORM
-//   contextp->timeInc(1); // necessary for wave gen
-//   tfp->dump(contextp->time());
+//   if(!first_flag) { // 打印还没有 eval() 时的波形
+//     tfp->dump(contextp->time()); // 此时还没有调用过 context->timeInc()， 这里是在时间 0 画上一个边
+//     first_flag = 1; // 默认时间0时，clock = 1
+//   }
 // #endif
+// 产生一个时钟下沿，然后计算这个下沿的信号变化(猜测：组合信号不受影响，时序信号也不受影响（因为我没有negedge)
+  top->clock = 0; top->eval(); // 表示在时间 0, clock = 0, 然后用eval()计算各个信号的值(估计此时只能计算组合电路)    在第二次执行时，这里就表示下降沿了
+#ifdef CONFIG_WAVEFORM
+  tfp->dump(contextp->time()); // 把时间 0 的信号电平画下来
+  contextp->timeInc(1); // 时间前进1
+#endif
+  // void dealWithExit();
+  // dealWithExit();
+  top->clock = 1; top->eval(); // 在时间 +1 时， clock = 1，这是一个上升沿。调用 eval() 计算各个信号的值 （更新上升沿的时序逻辑，以及会受到响应影响的组合逻辑）
+#ifdef CONFIG_WAVEFORM
+  tfp->dump(contextp->time()); // 画出这个时间的信号电平
+  contextp->timeInc(1); // 时间前进1
+#endif
 }
 
 
