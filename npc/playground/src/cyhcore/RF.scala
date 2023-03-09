9551167f249bf2f5f5096a6820622910fb2952a6
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-09 16:58:58 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:58:58 up 2 days,  5:25,  4 users,  load average: 0.39, 0.18, 0.19

--- npc/playground/src/cyhcore/RF.scala
+++ npc/playground/src/cyhcore/RF.scala
@@ -7,11 +7,11 @@ trait HasRegFileParameter {
   val NRReg = 32
 }
 
-class RegFile extends HasRegFileParameter with HasNutCoreParameter {
-  val rf = Mem(NRReg, UInt(XLEN.W))
-  def read(addr: UInt) : UInt = Mux(addr === 0.U, 0.U, rf(addr))
-  def write(addr: UInt, data: UInt) = { rf(addr) := data(XLEN-1,0) }
-} 
+// class RegFile extends HasRegFileParameter with HasNutCoreParameter {
+//   val rf = Mem(NRReg, UInt(XLEN.W))
+//   def read(addr: UInt) : UInt = Mux(addr === 0.U, 0.U, rf(addr))
+//   def write(addr: UInt, data: UInt) = { rf(addr) := data(XLEN-1,0) }
+// } 
 
 
 
