034a32bbeadd2c8e2fa15ead4c32892df8889fc6
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-09 19:07:21 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:07:21 up 2 days,  7:34,  4 users,  load average: 0.38, 0.32, 0.22

--- npc/playground/src/cyhcore/Decode.scala
+++ npc/playground/src/cyhcore/Decode.scala
@@ -6,6 +6,19 @@ import chisel3.util._
 import Macros._
 import Macros.Constants._
 
+object FuType {
+  def alu = 0.U(log2Up(num).W)
+  def lsu = 1.U(3.W)
+  def mdu = 2.U(3.W)
+  // def csr = 3.U(3.W)
+  // def mou = 4.U(3.W)
+  // def bru = if(IndependentBru) "b101".U
+  //           else               alu
+  def bru = alu
+  def num = 3
+  def apply() = UInt(log2Up(num).W)
+}
+
 object Instructions {
   def NOP = 0x00000013.U
   val DecodeDefault = List(N, BR_N , OP1_X  , OP2_X  , ALU_X   , WB_X  , WREG_0, WMEM_0, MEM_MSK_X , ALU_MSK_X, SIGN_X)
