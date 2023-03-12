5c66eb193da43f4a999a4df67b3658f9138bc25f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-12 22:41:16 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:41:16 up 1 day,  5:16,  1 user,  load average: 0.46, 0.27, 0.15

--- npc/playground/src/utils/OneHotTree.scala
+++ npc/playground/src/utils/OneHotTree.scala
@@ -1,19 +1,19 @@
-// package utils
-// 
-// import chisel3._
-// import chisel3.util._
-// 
-// // 因此，在选择编码方式时，应该根据具体应用情况综合考虑各种因素。
-// // 一般来说，在选择信号的位数较少的情况下，一热编码选择器是更好的选择。
-// // 但是，在选择信号的位数较多时，一般选择器可能更加适合。
-// // TODO: 单热点的好处？为什么好？
-// object OneHotTree {
-//   // [T<:Data] 有点类似于C++中的模板，定义一个模板类型T,这个T只能是Data的派生类
-//   def apply[T <: Data](key: UInt, mapping: Iterable[(UInt, T)]): T =
-//     Mux1H(mapping.map(p => (p._1 === key, p._2)))
+package utils
+
+import chisel3._
+import chisel3.util._
+
+// 因此，在选择编码方式时，应该根据具体应用情况综合考虑各种因素。
+// 一般来说，在选择信号的位数较少的情况下，一热编码选择器是更好的选择。
+// 但是，在选择信号的位数较多时，一般选择器可能更加适合。
+// TODO: 单热点的好处？为什么好？
+object OneHotTree {
+  // [T<:Data] 有点类似于C++中的模板，定义一个模板类型T,这个T只能是Data的派生类
+  def apply[T <: Data](key: UInt, mapping: Iterable[(UInt, T)]): T =
+    Mux1H(mapping.map(p => (p._1 === key, p._2)))
+}
+
+// object LookupTreeDefault {
+//   def apply[T <: Data](key: UInt, default: T, mapping: Iterable[(UInt, T)]): T =
+//     MuxLookup(key, default, mapping.toSeq)
 // }
-// 
-// // object LookupTreeDefault {
-// //   def apply[T <: Data](key: UInt, default: T, mapping: Iterable[(UInt, T)]): T =
-// //     MuxLookup(key, default, mapping.toSeq)
-// // }
