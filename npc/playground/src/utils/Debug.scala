72ec13e8a26cec6c48eed39a670e2f59d0b7d9d2
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-13 17:13:07 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:13:07 up 1 day, 15:20,  4 users,  load average: 0.26, 0.27, 0.26

--- npc/playground/src/utils/Debug.scala
+++ npc/playground/src/utils/Debug.scala
@@ -12,7 +12,7 @@ object Debug {
 
   def apply(pable: Printable) {
     if(debug)
-      printf("[Cycle: %ld]", CTimer())
+      // printf("[Cycle: %ld]", CTimer())
       printf(pable)
       printf("\n")
   }
