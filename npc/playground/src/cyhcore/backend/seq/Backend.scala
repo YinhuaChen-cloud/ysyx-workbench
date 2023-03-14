430eba88083c3c757f42f944af100ba788b0a72e
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-14 17:18:04 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:18:04 up  6:50,  3 users,  load average: 0.19, 0.35, 0.41

--- npc/playground/src/cyhcore/backend/seq/Backend.scala
+++ npc/playground/src/cyhcore/backend/seq/Backend.scala
@@ -8,12 +8,14 @@ class Backend extends CyhCoreModule {
     val in = Flipped(new DecodeIO)
   })
 
-  val isu  = Module(new ISU) // 发射队列，目前只是用于存放 regfile，以及负责regfile的读写
   val exu = Module(new EXU)
   val wbu  = Module(new WBU)
+  val isu  = Module(new ISU) // 发射队列，目前只是用于存放 regfile，以及负责regfile的读写
 
   io.in <> exu.io.in
   exu.io.out <> wbu.io.in
+  wbu.io.wb <> isu.io.wb
+  
 
   // PipelineConnect(isu.io.out, exu.io.in, exu.io.out.fire(), io.flush(0))
   // PipelineConnect(exu.io.out, wbu.io.in, true.B, io.flush(1))
