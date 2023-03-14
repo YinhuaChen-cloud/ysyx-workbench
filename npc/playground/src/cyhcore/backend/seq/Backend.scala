abda67ab9ff5f918c503e55c7dcd087531bbe880
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-14 19:33:46 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:33:46 up  9:06,  3 users,  load average: 0.45, 0.19, 0.12

--- npc/playground/src/cyhcore/backend/seq/Backend.scala
+++ npc/playground/src/cyhcore/backend/seq/Backend.scala
@@ -8,15 +8,20 @@ class Backend extends CyhCoreModule {
     val in = Flipped(new DecodeIO)
   })
 
+  // 根据果壳的代码，后端单元顺序应该如下
+  val isu  = Module(new ISU) // 发射队列，目前只是用于存放 regfile，以及负责regfile的读写
   val exu = Module(new EXU)
   val wbu  = Module(new WBU)
-  val isu  = Module(new ISU) // 发射队列，目前只是用于存放 regfile，以及负责regfile的读写
 
-  io.in <> exu.io.in
+  // 一个环 io -> isu -> exu -> wbu
+  //               ^             |
+  //               |             |
+  //               |-------------|
+  io.in <> isu.io.in
+  isu.io.out <> exu.io.in
   exu.io.out <> wbu.io.in
-  wbu.io.wb <> isu.io.wb
+  wbu.io.wb  <> isu.io.wb
   
-
   // PipelineConnect(isu.io.out, exu.io.in, exu.io.out.fire(), io.flush(0))
   // PipelineConnect(exu.io.out, wbu.io.in, true.B, io.flush(1))
 
--- npc/playground/src/cyhcore/backend/seq/ISU.scala
+++ npc/playground/src/cyhcore/backend/seq/ISU.scala
@@ -17,7 +17,51 @@ class ISU extends CyhCoreModule with HasRegFileParameter {
   // write rf
   when (io.wb.rfWen) { rf.write(io.wb.rfDest, io.wb.rfData) }
 
-  io.out := DontCare
-  
+// out(DecodeIO) -------------------------------------- cf(CtrlFlowIO)
+//   val instr = Output(UInt(64.W))
+//   val pc = Output(UInt(VAddrBits.W))
+//   val redirect = new RedirectIO  // TODO: 我猜测这个应该是用来处理跳转指令的
+
+  io.in.cf <> io.out.cf
+
+// out(DecodeIO) -------------------------------------- ctrl(CtrlSignalIO)
+//   val src1Type = Output(SrcType())
+//   val src2Type = Output(SrcType())
+//   val fuType = Output(FuType())
+//   val fuOpType = Output(FuOpType())
+//   val rfSrc1 = Output(UInt(5.W))     // 来自 Decoder 的 rf1 addr
+//   val rfSrc2 = Output(UInt(5.W))     // 来自 Decoder 的 rf2 addr
+//   val rfWen = Output(Bool())         // 是否写入寄存器
+//   val rfDest = Output(UInt(5.W))     // 写入的目标寄存器的 addr
+
+  io.out.ctrl <> io.in.ctrl
+
+// out(DecodeIO) -------------------------------------- data(DataSrcIO)
+//   val src1 = Output(UInt(XLEN.W))
+//   val src2 = Output(UInt(XLEN.W))
+//   val imm  = Output(UInt(XLEN.W))
+
+  val rfSrc1 = io.in.ctrl.rfSrc1
+  val rfSrc2 = io.in.ctrl.rfSrc2
+  val rfDest = io.in.ctrl.rfDest
+
+  io.out.data.src1 := Mux1H(List(
+    (io.in.ctrl.src1Type === SrcType.pc)                                            -> SignExt(io.in.cf.pc, PC_LEN), // TODO: 为什么用 pc 作为src1要进行有符号扩展？ 为什么不是无符号扩展？
+    // src1ForwardNextCycle                                                            -> io.forward.wb.rfData, //io.forward.wb.rfData,
+    // (src1Forward && !src1ForwardNextCycle)                                          -> io.wb.rfData, //io.wb.rfData,
+    // ((io.in.ctrl.src1Type =/= SrcType.pc) && !src1ForwardNextCycle && !src1Forward) -> rf.read(rfSrc1)
+    (io.in.ctrl.src1Type =/= SrcType.pc)                                            -> rf.read(rfSrc1)
+  ))
+
+  io.out.data.src2 := Mux1H(List(
+    (io.in.ctrl.src2Type =/= SrcType.reg)                                           -> io.in.data.imm, // TODO: 为什么我们仍然需要 io.out.data.imm 端口？
+    // src2ForwardNextCycle                                                             -> io.forward.wb.rfData, //io.forward.wb.rfData,
+    // (src2Forward && !src2ForwardNextCycle)                                           -> io.wb.rfData, //io.wb.rfData,
+    // ((io.in.ctrl.src2Type === SrcType.reg) && !src2ForwardNextCycle && !src2Forward) -> rf.read(rfSrc2)
+    (io.in.ctrl.src2Type === SrcType.reg)                                           -> rf.read(rfSrc2)
+  ))
+
+  io.out.data.imm  := io.in.data.imm
+
 }
 
