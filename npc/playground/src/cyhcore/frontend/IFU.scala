ea89a3dff56c37476049039e959e0e865dc147cc
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 21:42:04 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:42:04 up  6:36,  3 users,  load average: 0.64, 0.43, 0.29

--- npc/playground/src/cyhcore/frontend/IFU.scala
+++ npc/playground/src/cyhcore/frontend/IFU.scala
@@ -53,6 +53,7 @@ class IFU extends CyhCoreModule with HasResetVector {
   // val addr = Output(UInt(PAddrBits.W)) // 访存地址（位宽与体系结构实现相关）, 默认 32 位
 
   io.imem.req.addr  := pc_reg
+  io.imem.req.cmd   := SimpleBusCmd.read
 
 // out(CtrlFlowIO) ------------------------------------------ 
   // val instr = Output(UInt(64.W))
