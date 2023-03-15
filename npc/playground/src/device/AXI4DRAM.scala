28bf6760e16f3727722fb5c2ba8500b2472f91be
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 21:39:32 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:39:32 up  6:34,  3 users,  load average: 0.52, 0.38, 0.26

--- npc/playground/src/device/AXI4DRAM.scala
+++ npc/playground/src/device/AXI4DRAM.scala
@@ -1,83 +1,83 @@
-package device
+// package device
 
-import chisel3._
-import chisel3.util._
+// import chisel3._
+// import chisel3.util._
 
-import cyhcore._
-import bus.simplebus._
+// import cyhcore._
+// import bus.simplebus._
 
-class AXI4DRAM extends CyhCoreModule {
-  val io = IO(new Bundle {
-    val dmem = Flipped(new SimpleBusUC)  // DRAM 属于从模块，因此要 Flipped
-  })
+// class AXI4DRAM extends CyhCoreModule {
+//   val io = IO(new Bundle {
+//     val dmem = Flipped(new SimpleBusUC)  // DRAM 属于从模块，因此要 Flipped
+//   })
 
-  val rwmem = Module(new RWMEM)
-  rwmem.io.clk  := clock
-  rwmem.io.rst  := reset
+//   val rwmem = Module(new RWMEM)
+//   rwmem.io.clk  := clock
+//   rwmem.io.rst  := reset
 
-  rwmem.io.isRead    := io.dmem.req.isRead()
-  io.dmem.resp.rdata := rwmem.io.rdata
+//   rwmem.io.isRead    := io.dmem.req.isRead()
+//   io.dmem.resp.rdata := rwmem.io.rdata
 
-  rwmem.io.addr  := io.dmem.req.addr
+//   rwmem.io.addr  := io.dmem.req.addr
 
-  rwmem.io.isWrite := io.dmem.req.isWrite()
-  rwmem.io.mem_write_data := io.dmem.req.wdata
-  rwmem.io.mem_write_msk  := io.dmem.req.wmask
+//   rwmem.io.isWrite := io.dmem.req.isWrite()
+//   rwmem.io.mem_write_data := io.dmem.req.wdata
+//   rwmem.io.mem_write_msk  := io.dmem.req.wmask
 
-}
+// }
 
-class RWMEM extends BlackBox with HasBlackBoxInline with HasCyhCoreParameter {
-  val io = IO(new Bundle {
-    val clk = Input(Clock())
-    val rst = Input(Bool())
-    // read
-    val isRead = Input(Bool())
-    val rdata = Output(UInt(XLEN.W))
-    // addr for r/w
-    val addr = Input(UInt(PAddrBits.W))
-    // write
-    val isWrite = Input(Bool())
-    val mem_write_data = Input(UInt(XLEN.W))
-    val mem_write_msk = Input(UInt(8.W))
-  })
+// class RWMEM extends BlackBox with HasBlackBoxInline with HasCyhCoreParameter {
+//   val io = IO(new Bundle {
+//     val clk = Input(Clock())
+//     val rst = Input(Bool())
+//     // read
+//     val isRead = Input(Bool())
+//     val rdata = Output(UInt(XLEN.W))
+//     // addr for r/w
+//     val addr = Input(UInt(PAddrBits.W))
+//     // write
+//     val isWrite = Input(Bool())
+//     val mem_write_data = Input(UInt(XLEN.W))
+//     val mem_write_msk = Input(UInt(8.W))
+//   })
 
-  setInline("RWMEM.v",
-            s"""
-              |module AXI4DRAM (
-              |           input clk,
-              |           input rst,
-              |
-              |           input isRead,
-              |           output reg [${XLEN}-1:0] rdata,
-              |
-              |           input [${PAddrBits}-1:0] addr,
-              |
-              |           input isWrite,
-              |           input [${XLEN}-1:0] mem_write_data,
-              |           input [7:0] mem_write_msk);
-              |
-              |  wire [${XLEN}-1:0] mem_addr;
-              |  assign mem_addr[${PAddrBits}-1:0] = addr;
-              |
-              |  // for mem_rw
-              |  import "DPI-C" function void pmem_read(input longint raddr, output longint rdata);
-              |  import "DPI-C" function void pmem_write(input longint waddr, input longint wdata, input byte wmask);
-              |  // for data reading from mem
-              |  always@(*) begin
-              |    if(isRead)
-			        |      pmem_read(mem_addr, mem_in); 
-              |    else
-              |      mem_in = '0;
-              |  end
-              |  // for writing mem
-              |  always@(posedge clk) 
-              |    if(isWriteMem) 
-              |      pmem_write(mem_addr, mem_write_data, mem_write_msk);
-              |
-              |endmodule
-            """.stripMargin)
+//   setInline("RWMEM.v",
+//             s"""
+//               |module AXI4DRAM (
+//               |           input clk,
+//               |           input rst,
+//               |
+//               |           input isRead,
+//               |           output reg [${XLEN}-1:0] rdata,
+//               |
+//               |           input [${PAddrBits}-1:0] addr,
+//               |
+//               |           input isWrite,
+//               |           input [${XLEN}-1:0] mem_write_data,
+//               |           input [7:0] mem_write_msk);
+//               |
+//               |  wire [${XLEN}-1:0] mem_addr;
+//               |  assign mem_addr[${PAddrBits}-1:0] = addr;
+//               |
+//               |  // for mem_rw
+//               |  import "DPI-C" function void pmem_read(input longint raddr, output longint rdata);
+//               |  import "DPI-C" function void pmem_write(input longint waddr, input longint wdata, input byte wmask);
+//               |  // for data reading from mem
+//               |  always@(*) begin
+//               |    if(isRead)
+// 			        |      pmem_read(mem_addr, mem_in); 
+//               |    else
+//               |      mem_in = '0;
+//               |  end
+//               |  // for writing mem
+//               |  always@(posedge clk) 
+//               |    if(isWriteMem) 
+//               |      pmem_write(mem_addr, mem_write_data, mem_write_msk);
+//               |
+//               |endmodule
+//             """.stripMargin)
 
-}
+// }
 
 
 
