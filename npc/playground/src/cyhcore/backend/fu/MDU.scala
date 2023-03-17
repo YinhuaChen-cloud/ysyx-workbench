93ab9d2244274fcc4bf632c8b2b5bd0f55b6bd66
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-17 20:21:15 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:21:15 up 13:55,  4 users,  load average: 0.00, 0.05, 0.07

--- npc/playground/src/cyhcore/backend/fu/MDU.scala
+++ npc/playground/src/cyhcore/backend/fu/MDU.scala
@@ -6,24 +6,32 @@ import chisel3.util._
 // import Macros._
 // import Macros.Constants._
 
-object MDUOpType {
-  def mul    = "b0000".U
-  def mulh   = "b0001".U
-  def mulhsu = "b0010".U
-  def mulhu  = "b0011".U
-  def div    = "b0100".U
-  def divu   = "b0101".U
-  def rem    = "b0110".U
-  def remu   = "b0111".U
+import utils._
 
+object MDUOpType {
+  // MUL performs an XLEN-bit×XLEN-bit multiplication of rs1 by rs2 and places the 
+  // lower XLEN bits in the destination register
+  def mul    = "b0000".U
+  // def mulh   = "b0001".U
+  // def mulhsu = "b0010".U
+  // def mulhu  = "b0011".U
+  // def div    = "b0100".U
+  def divu   = "b0101".U
+  // def rem    = "b0110".U
+  // def remu   = "b0111".U
+
+  // MULW is an RV64 instruction that multiplies the lower 32 bits of the source registers, 
+  // placing the sign-extension of the lower 32 bits of the result into the destination register.
   def mulw   = "b1000".U
   def divw   = "b1100".U
   def divuw  = "b1101".U
   def remw   = "b1110".U
-  def remuw  = "b1111".U
+  // def remuw  = "b1111".U
 
-  def isDiv(op: UInt) = op(2)
-  def isDivSign(op: UInt) = isDiv(op) && !op(0)
+  // 求余可以用除法器来计算，所以这两个做同一个判断
+  def isDivRem(op: UInt) = op(2)
+  def isDivRemSign(op: UInt) = isDivRem(op) && !op(0)
+  def isRem(op: UInt) = op(1)
   def isW(op: UInt) = op(3)
 }
 
@@ -35,20 +43,85 @@ object MDUOpType {
 //   val result  = Output(UInt(conf.xlen.W))
 // }
 
-// // DIV, MUL, REM
-// class MDU (implicit val conf: Configuration) extends Module {
-//   val io = IO(new MDU_bundle())
+class MulDivIO(val len: Int) extends Bundle {
+  val in = Flipped(Vec(2, Output(UInt(len.W)))) // 两个输入
+  val sign = Input(Bool()) // 是否有符号
+  val out = Output(UInt((len * 2).W)) // 乘法结果的位宽有可能是输入位宽的2倍
+}
 
-//   io.result := MuxCase(
-//     0.U, Array(
-//       (io.alu_op === ALU_DIV && io.alu_msk_type =/= ALU_MSK_W)    -> (io.alu_op1.asSInt / io.alu_op2.asSInt).asUInt,
-//       (io.alu_op === ALU_DIV && io.alu_msk_type === ALU_MSK_W)    -> (io.alu_op1(31, 0).asSInt / io.alu_op2(31, 0).asSInt).asUInt,
-//       (io.alu_op === ALU_REM && io.alu_msk_type =/= ALU_MSK_W)    -> (io.alu_op1.asSInt % io.alu_op2.asSInt).asUInt(),
-//       (io.alu_op === ALU_REM && io.alu_msk_type === ALU_MSK_W)    -> (io.alu_op1(31, 0).asSInt % io.alu_op2(31, 0).asSInt).asUInt,
-//     )
-//   )
+class Multiplier(len: Int) extends CyhCoreModule {
+  val io = IO(new MulDivIO(len))
 
-// }
+  val mulRes = io.in(0).asSInt * io.in(1).asSInt
+  io.out := mulRes.asUInt
+}
+
+class Divider(len: Int) extends CyhCoreModule {
+  val io = IO(new MulDivIO(len))
+
+  val (sign, src1, src2) = (io.sign, io.in(0), io.in(1))
+  // def abs(a: UInt, sign: Bool): (Bool, UInt) = {
+  //   val s = a(len - 1) && sign
+  //   (s, Mux(s, -a, a))
+  // }
+
+  val resQ = Wire(UInt(len.W))
+  resQ := Mux(sign, src1.asSInt / src2.asSInt, src1.asUInt / src2.asUInt)
+  val resR = Wire(UInt(len.W))
+  resR := Mux(sign, src1.asSInt % src2.asSInt, src1.asUInt % src2.asUInt)
+  io.out := Cat(resR, resQ)
+
+  // io.result := MuxCase(
+  //   0.U, Array(
+  //     (io.alu_op === ALU_DIV && io.alu_msk_type =/= ALU_MSK_W)    -> (io.alu_op1.asSInt / io.alu_op2.asSInt).asUInt,
+  //     (io.alu_op === ALU_DIV && io.alu_msk_type === ALU_MSK_W)    -> (io.alu_op1(31, 0).asSInt / io.alu_op2(31, 0).asSInt).asUInt,
+  //     (io.alu_op === ALU_REM && io.alu_msk_type =/= ALU_MSK_W)    -> (io.alu_op1.asSInt % io.alu_op2.asSInt).asUInt(),
+  //     (io.alu_op === ALU_REM && io.alu_msk_type === ALU_MSK_W)    -> (io.alu_op1(31, 0).asSInt % io.alu_op2(31, 0).asSInt).asUInt,
+  //   )
+  // )
+
+}
+
+class MDUIO extends FunctionUnitIO {
+}
+
+// DIV, MUL, REM
+class MDU extends CyhCoreModule {
+  // val io = IO(new MDU_bundle())
+  val io = IO(new MDUIO)
+
+  val (src1, src2, func) = (io.in.src1, io.in.src2, io.in.func)
+  def access(src1: UInt, src2: UInt, func: UInt): UInt = {
+    this.src1  := src1
+    this.src2  := src2
+    this.func  := func
+    io.out
+  }
+
+  // 判断使用除法器还是乘法器
+  val isDivRem = MDUOpType.isDivRem(func)
+  // 判断是否是求余
+  val isRem = MDUOpType.isRem(func)
+  // 如果是除法，是有符号还是无符号    NOTE: 乘法不需要判断符号
+  val isDivRemSign = MDUOpType.isDivRemSign(func)
+  // 判断是否是 half 计算
+  val isW = MDUOpType.isW(func)
+
+  // TODO: 为什么果壳这里乘法器的长度是 XLEN + 1，不是XLEN?
+  val mul = Module(new Multiplier(XLEN)) 
+  mul.io.in(0) := src1
+  mul.io.in(1) := src2
+  mul.io.sign  := isDivRemSign // NOTE: 其实乘法不需要判断符号
+  val mulRes = mul.io.out(XLEN-1,0)   // mul 把XLEN*XLEN后，只取XLEN位宽的结果
+
+  val div = Module(new Divider(XLEN)) // 除法器的结果位宽是2*XLEN，低XLEN位放商，高XLEN位放余
+  val divRes = Mux(isRem, div.io.out(2*XLEN-1,XLEN), div.io.out(XLEN-1,0)) // 除法的商放在低XLEN位，余数放在高XLEN位
+
+  val res = Mux(isDivRem, divRes, mulRes)
+
+  io.out := Mux(isW, SignExt(res(31,0),XLEN), res)  // 根据数学公式证明，mulw 直接取 mul 结果的低32位即可
+
+}
 
 
 
