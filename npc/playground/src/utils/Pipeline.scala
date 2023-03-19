6911864c37e134ed6abc71a03309beee62deb075
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-19 20:24:25 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:24:25 up 1 day, 14:27,  3 users,  load average: 0.44, 0.30, 0.20

--- npc/playground/src/utils/Pipeline.scala
+++ npc/playground/src/utils/Pipeline.scala
@@ -6,7 +6,7 @@ import chisel3.util._
 // 当前假设：划分的每一个阶段，都能在一拍内完成自己的功能
 // IFU -> IDU reg -> IDU -> EXU reg -> EXU -> WBU reg -> WBU (不考虑跳转指令)
 object PipelineConnect {
-  def apply[T <: Data](left: T, right: T) = {
+  def apply[T <: Data](left: T, right: DecoupledIO[T]) = {
     // 每一回合都有新数据写入，每一回合数据都有效
 
     val valid = RegInit(false.B)
@@ -14,7 +14,8 @@ object PipelineConnect {
 
     val regs = RegEnable(left, true.B)
 
-    right := regs
+    right.bits := regs
+    right.valid := valid
 
   }
 }
