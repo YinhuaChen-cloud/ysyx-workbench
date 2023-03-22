72831825f9bc6b110ef68a8aa0e9fad4d34f918e
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-22 15:05:54 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:05:54 up 3 days, 14:04,  7 users,  load average: 3.56, 2.03, 0.92

--- npc/playground/src/cyhcore/frontend/IFU.scala
+++ npc/playground/src/cyhcore/frontend/IFU.scala
@@ -52,7 +52,7 @@ class IFU extends CyhCoreModule with HasResetVector {
 
 // --- Jump wire of inst to ALU, for calculating next_pc in time ---
 
-  BoringUtils.addSource(io.imem.resp.rdata, "real_inst2")
+  BoringUtils.addSource(io.imem.resp.rdata, "real_inst")
 
   Debug("In IFU, The inst read is 0x%x", io.imem.resp.rdata)
 
