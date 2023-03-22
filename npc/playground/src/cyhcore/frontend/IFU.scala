1afed7f4a96a5049ca218de413cad3ad59bcefd7
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-22 15:57:08 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:57:08 up 3 days, 14:55,  7 users,  load average: 0.23, 0.25, 0.22

--- npc/playground/src/cyhcore/frontend/IFU.scala
+++ npc/playground/src/cyhcore/frontend/IFU.scala
@@ -24,13 +24,17 @@ class IFU extends CyhCoreModule with HasResetVector {
   val pc_reg = RegInit(resetVector.U(PC_LEN.W)) // TODO：果壳里，PC寄存器的长度是39
   val bpu = Module(new BPU)
   bpu.io.instr := io.imem.resp.rdata(INST_LEN-1, 0) // io.imem.resp.rdata 刚刚读入的指令
+  // 表示下一条指令是否是NOP
+  val isNOP = RegInit(false.B)
+  // 如果当前取到的指令是 branch/jmp，那么isNOP置 1，表示下一条指令是NOP
+  isNOP := Mux(bpu.io.isBranchJmp, true.B, false.B)
 
   // io.redirect.valid, io.redirect.target 需要在第二拍才能计算出来
   // 思路：实现一个小译码，判断当前读到的指令（发送给下一级的指令）是否是branch指令（jmp属于无条件跳转指令）
-  // 如果是branch，则阻塞流水线，直到io.redirect.valid为true，并且取用io.redirect.target
-  // 如果是jmp，也是阻塞流水线，...... TODO：无条件跳转指令应该能进一步优化
+  // 如果是branch，则阻塞流水线(pc不变，下一周期发送NOP)，直到io.redirect.valid为true，并且取用io.redirect.target
+  // 如果是jmp，也是阻塞流水线(pc不变，下一周期发送NOP)，...... TODO：无条件跳转指令应该能进一步优化
   // 如果不是，就 PC + 4
-  pc_reg := Mux(bpu.io.isBranchJmp, Mux(io.redirect.valid, io.redirect.target, pc_reg), pc_reg + 4.U) 
+  pc_reg := Mux(isNOP, pc_reg, Mux(bpu.io.isBranchJmp, Mux(io.redirect.valid, io.redirect.target, pc_reg), pc_reg + 4.U))
   // pc_reg := Mux(io.redirect.valid, io.redirect.target, pc_reg) 
   // pc_reg := pc_reg + 4.U // TODO: 先不考虑跳转指令
 
@@ -47,7 +51,8 @@ class IFU extends CyhCoreModule with HasResetVector {
   // val redirect = new RedirectIO  
 
   io.out       := DontCare
-  io.out.instr := Mux(bpu.io.isBranchJmp, Instructions.NOP, io.imem.resp.rdata)(INST_LEN-1, 0)
+  // 如果发现 isNOP = true.B，说明这一回合应该发送气泡指令，而非真正的指令，真正的指令需要保留一回合
+  io.out.instr := Mux(isNOP, Instructions.NOP, io.imem.resp.rdata)(INST_LEN-1, 0)
   io.out.pc    := pc_reg
 
 // --- Jump wire of inst to ALU, for calculating next_pc in time ---
