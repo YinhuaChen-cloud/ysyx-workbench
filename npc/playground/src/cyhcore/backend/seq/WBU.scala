67a3dba5a114a7a7e9365af1b962fbffa023ec9d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-22 21:58:20 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:58:20 up 3 days, 20:51,  3 users,  load average: 0.23, 0.32, 0.28

--- npc/playground/src/cyhcore/backend/seq/WBU.scala
+++ npc/playground/src/cyhcore/backend/seq/WBU.scala
@@ -9,7 +9,7 @@ import chisel3.util.experimental.BoringUtils
 // 用来把结果写回 寄存器堆 和 PC   TODO: 从端口来看，似乎不写回内存
 class WBU extends CyhCoreModule { // ------------- halfchecked
   val io = IO(new Bundle {
-    val in = Flipped(Decoupled(new CommitIO))
+    val in = Flipped(new CommitIO)
     val wb = new WriteBackIO // 写回寄存器用的端口
     val redirect = new RedirectIO
   })
@@ -19,27 +19,28 @@ class WBU extends CyhCoreModule { // ------------- halfchecked
   // val rfDest = Output(UInt(5.W))
   // val rfData = Output(UInt(XLEN.W))
 
-  io.wb.rfWen  := io.in.bits.decode.ctrl.rfWen & io.in.valid
-  io.wb.rfDest := io.in.bits.decode.ctrl.rfDest
-  io.wb.rfData := io.in.bits.commits(io.in.bits.decode.ctrl.fuType) // EXU 四个功能单元的输出都在commit，让 WBU 挑选
+  // io.wb.rfWen  := io.in.decode.ctrl.rfWen & io.in.valid
+  io.wb.rfWen  := io.in.decode.ctrl.rfWen
+  io.wb.rfDest := io.in.decode.ctrl.rfDest
+  io.wb.rfData := io.in.commits(io.in.decode.ctrl.fuType) // EXU 四个功能单元的输出都在commit，让 WBU 挑选
 
 // redirect(RedirectIO) ------------------------------------------
   // val target = Output(UInt(PC_LEN.W))
 
   dontTouch(io.redirect)
-  dontTouch(io.in.bits.decode.cf.redirect)
-  io.in.bits.decode.cf.redirect <> io.redirect
+  dontTouch(io.in.decode.cf.redirect)
+  io.in.decode.cf.redirect <> io.redirect
   // io.redirect.valid := io.in.bits.decode.cf.redirect.valid && io.in.valid
 
 // handshake ------------------------------------------ 
   
-  io.in.ready  := DontCare
+  // io.in.ready  := DontCare
 
 // for difftest ---------------------------------------
   // 当 io.in.valid 为 true 时，说明下一周期寄存器堆就会被写入（WBU和ISU之间没有流水线）
   // 所以，可以在下一个时钟上升沿启用 difftest
   // TODO: 上板的时候这个应该得去掉(果壳去掉了)
-  BoringUtils.addSource(RegNext(io.in.valid), "difftestCommit")
+  // BoringUtils.addSource(RegNext(io.in.valid), "difftestCommit")
 
 }
 
--- npc/playground/src/sim/CyhSocSimTop.scala
+++ npc/playground/src/sim/CyhSocSimTop.scala
@@ -34,28 +34,28 @@ class CyhSocSimTop extends CyhCoreModule with HasRegFileParameter {
   cyhsoc.io.imem <> axi4sram.io.imem
   cyhsoc.io.dmem <> axi4dram.io.dmem
 
-  // difftest ------------------- start 
-  // 当 io.in.valid 为 true 时，说明目前的信号会在下一个时钟上升沿起效果（写入寄存器、写入内存）
-  // 因此 difftest 也要在下一个时钟上升沿去做
-  // 一个例外：当接收到的指令为NOP时，下一个时钟上升沿不能做difftest
-  val difftestCommit = WireInit(false.B)
-  BoringUtils.addSink(difftestCommit, "difftestCommit")
+  // // difftest ------------------- start 
+  // // 当 io.in.valid 为 true 时，说明目前的信号会在下一个时钟上升沿起效果（写入寄存器、写入内存）
+  // // 因此 difftest 也要在下一个时钟上升沿去做
+  // // 一个例外：当接收到的指令为NOP时，下一个时钟上升沿不能做difftest
+  // val difftestCommit = WireInit(false.B)
+  // BoringUtils.addSink(difftestCommit, "difftestCommit")
 
-  val difftest = Module(new DiffTest)
-  difftest.io.clk    := clock
-  difftest.io.rst    := reset
-  difftest.io.commit := difftestCommit
-  difftest.io.pc     := DontCare // 现在先不对比 pc
+  // val difftest = Module(new DiffTest)
+  // difftest.io.clk    := clock
+  // difftest.io.rst    := reset
+  // difftest.io.commit := difftestCommit
+  // difftest.io.pc     := DontCare // 现在先不对比 pc
 
-  // val difftest_valid = RegNext(io.in.valid & (io.in.bits.cf.instr =/= Instructions.NOP)) // 告诉仿真环境可以做difftest了
-  val rf = WireInit(VecInit(Seq.fill(NRReg)(0.U(XLEN.W))))
-  BoringUtils.addSink(rf, "difftestRegs")
-  val rf_aux = Wire(Vec(NRReg * XLEN, Bool()))
-  for(i <- 0 until NRReg) {
-    rf_aux.slice(i * XLEN, (i+1) * XLEN).zip(rf(i).asBools).foreach{case (a, b) => a := b}
-  }
-  difftest.io.regfile := rf_aux.asUInt
-  // difftest ------------------- end
+  // // val difftest_valid = RegNext(io.in.valid & (io.in.bits.cf.instr =/= Instructions.NOP)) // 告诉仿真环境可以做difftest了
+  // val rf = WireInit(VecInit(Seq.fill(NRReg)(0.U(XLEN.W))))
+  // BoringUtils.addSink(rf, "difftestRegs")
+  // val rf_aux = Wire(Vec(NRReg * XLEN, Bool()))
+  // for(i <- 0 until NRReg) {
+  //   rf_aux.slice(i * XLEN, (i+1) * XLEN).zip(rf(i).asBools).foreach{case (a, b) => a := b}
+  // }
+  // difftest.io.regfile := rf_aux.asUInt
+  // // difftest ------------------- end
 
 }
 
