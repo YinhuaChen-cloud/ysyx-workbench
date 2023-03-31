e05e0142257ab4b848c23bf2a1c3e24f28327284
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-31 15:42:55 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:42:55 up 3 days, 23:46,  5 users,  load average: 0.43, 0.28, 0.27

--- npc/playground/src/cyhcore/backend/seq/WBU.scala
+++ npc/playground/src/cyhcore/backend/seq/WBU.scala
@@ -9,7 +9,7 @@ import chisel3.util.experimental.BoringUtils
 // 用来把结果写回 寄存器堆 和 PC   TODO: 从端口来看，似乎不写回内存
 class WBU extends CyhCoreModule { // ------------- halfchecked
   val io = IO(new Bundle {
-    val in = Flipped(new CommitIO)
+    val in = Flipped(Decoupled(new CommitIO))
     val wb = new WriteBackIO // 写回寄存器用的端口
     val redirect = new RedirectIO
     
@@ -21,22 +21,21 @@ class WBU extends CyhCoreModule { // ------------- halfchecked
   // val rfDest = Output(UInt(5.W))
   // val rfData = Output(UInt(XLEN.W))
 
-  // io.wb.rfWen  := io.in.decode.ctrl.rfWen & io.in.valid
-  io.wb.rfWen  := io.in.decode.ctrl.rfWen
-  io.wb.rfDest := io.in.decode.ctrl.rfDest
-  io.wb.rfData := io.in.commits(io.in.decode.ctrl.fuType) // EXU 四个功能单元的输出都在commit，让 WBU 挑选
+  io.wb.rfWen  := io.in.bits.decode.ctrl.rfWen & io.in.valid
+  io.wb.rfDest := io.in.bits.decode.ctrl.rfDest
+  io.wb.rfData := io.in.bits.commits(io.in.bits.decode.ctrl.fuType) // EXU 四个功能单元的输出都在commit，让 WBU 挑选
 
 // redirect(RedirectIO) ------------------------------------------
   // val target = Output(UInt(PC_LEN.W))
 
   dontTouch(io.redirect)
-  dontTouch(io.in.decode.cf.redirect)
-  io.in.decode.cf.redirect <> io.redirect
+  dontTouch(io.in.bits.decode.cf.redirect)
+  io.in.bits.decode.cf.redirect <> io.redirect
   // io.redirect.valid := io.in.bits.decode.cf.redirect.valid && io.in.valid
 
 // handshake ------------------------------------------ 
   
-  // io.in.ready  := DontCare
+  io.in.ready  := DontCare
 
 // for difftest ---------------------------------------
   // 当 io.in.valid 为 true 时，说明下一周期寄存器堆就会被写入（WBU和ISU之间没有流水线）
