b69626afd6f690dc6425e01fe692c46483d0bd8c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 02:34:20 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:34:20 up 5 days,  8:55,  6 users,  load average: 0.18, 0.28, 0.22

--- npc/playground/src/cyhcore/backend/seq/WBU.scala
+++ npc/playground/src/cyhcore/backend/seq/WBU.scala
@@ -35,13 +35,14 @@ class WBU extends CyhCoreModule { // ------------- halfchecked
   
   // 一拍内处理完输出，ready永远为true
   io.in.ready := true.B
+  // 注意：（WBU模块的输入端有可能会被阻塞，因此同一个输入只能写入一次（fire）
 
 // for difftest ---------------------------------------
   // 当 io.in.valid 为 true 时，说明下一周期寄存器堆就会被写入（WBU和ISU之间没有流水线）
   // 所以，可以在下一个时钟上升沿启用 difftest
   // TODO: 上板的时候这个应该得去掉(果壳去掉了)
   // 注意：在传入的指令为 NOP 时，由于spike并不执行NOP指令，所以不应该启动difftest
-  BoringUtils.addSource(RegNext(io.in.valid), "difftestCommit")
+  BoringUtils.addSource(RegNext(io.in.fire), "difftestCommit")
   // 用于difftest的PC应该是上一周期的（相对于被写入的寄存器）
   BoringUtils.addSource(SignExt(io.in.bits.decode.cf.pc, PC_LEN), "difftestThisPC")
 
