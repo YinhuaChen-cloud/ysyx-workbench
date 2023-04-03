ddb704f7786aacf23421a12ff93f4f8cdffa29fc
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 19:22:15 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:22:15 up 6 days,  1:43,  8 users,  load average: 0.65, 0.29, 0.15

--- npc/playground/src/cyhcore/backend/seq/ISU.scala
+++ npc/playground/src/cyhcore/backend/seq/ISU.scala
@@ -40,9 +40,8 @@ class ISU extends CyhCoreModule with HasRegFileParameter {
   // 每周期更新一遍busy数组(关于enable/disable，已经暗含在 idSetMask 和 wbClearMask里了)
   sb.update(idSetMask, wbClearMask)
   // 当 !src1Ready || !src2Ready 时，说明需要阻塞(除了WBU，都阻塞1周期)
+  val RAWhazard = !src1Ready || !src2Ready
   dontTouch(RAWhazard)
-  val RAWhazard = Wire(Bool())
-  RAWhazard := !src1Ready || !src2Ready
   BoringUtils.addSource(RAWhazard, "RAWhazard0")
 
 // out(DecodeIO) -------------------------------------- cf(CtrlFlowIO)
