8d28cc1b33a029d4d5039023392f7e16b12ae8c6
22050039-Yinhua Chen <12132329@mail.sustech.edu.cn>
2023-04-03 10:30:42 +0800

    2023-4-3. Before removing ready signal(nutshell style)

--- npc/playground/src/cyhcore/frontend/IDU.scala
+++ npc/playground/src/cyhcore/frontend/IDU.scala
@@ -81,10 +81,14 @@ class Decoder extends CyhCoreModule with HasInstrType {
   io.out.bits.data.imm := imm
 
 // handshake ------------------------------------------ 
-  
-  io.in.ready  := DontCare
-  // 由于假设IDU的运算能在一拍内完成，当 in 为 valid 时，就可以让 out 为 valid
-  io.out.valid := io.in.valid
+  // 一个前提：所有时序电路，最早也是在下个时钟上升沿才会读取我们的输出
+  // 因此，输出只要能在下个时钟上升沿之前准备好，就可以在下个时钟上升沿之前拉高valid（哪怕早于“数据真的准备好了”）
+
+  // 译码单元可以在valid为true的当时周期内把信号处理好，准备好输出(valid可拉高)
+  // 所以，译码单元的 ready 永远为 true，valid则和输入的valid同步
+
+  io.in.ready  := io.out.ready 
+  io.out.valid := io.in.valid && io.in.ready
 
   Debug(p"In IDU-Decoder, ${io.out.bits.ctrl}")
 
--- npc/playground/src/cyhcore/frontend/IFU.scala
+++ npc/playground/src/cyhcore/frontend/IFU.scala
@@ -21,14 +21,14 @@ class IFU extends CyhCoreModule with HasResetVector {
   })
 
   // pc
-  val pc_reg = RegInit(resetVector.U(PC_LEN.W)) // TODO：果壳里，PC寄存器的长度是39
+  val pc_reg = RegInit(resetVector.U(PC_LEN.W)) // TODO: 果壳里，PC寄存器的长度是39
   val bpu = Module(new BPU)
   bpu.io.instr := io.imem.resp.rdata(INST_LEN-1, 0) // imem.resp.rdata 是从 imem 中读取的真正的指令
   // 当isNOP为 1，表示当前周期要产生气泡指令
-  val isNOP = RegInit(false.B)
+  // val isNOP = RegInit(false.B)
   // 如果预译码发现当前指令是 branch/jmp，说明下一周期开始要产生气泡指令，因此置 isNOP reg 为 1
   // 例外：当 io.redirect.valid 为 true 时，说明下一周期 pc_reg 会跳转到正确的指令地址上，因此下一周期不需要再产生气泡指令
-  isNOP := bpu.io.isBranchJmp && !io.redirect.valid
+  // isNOP := bpu.io.isBranchJmp && !io.redirect.valid
 
   // io.redirect.valid, io.redirect.target 需要在第二拍才能计算出来
   // 思路：实现一个小译码，判断当前读到的指令（发送给下一级的指令）是否是branch指令（jmp属于无条件跳转指令）
@@ -36,7 +36,10 @@ class IFU extends CyhCoreModule with HasResetVector {
   // 如果是jmp，也是阻塞流水线(pc不变，下一周期发送NOP)，...... TODO：无条件跳转指令应该能进一步优化
   // 如果不是，就 PC + 4
   // 如果这一周期 bpu.io.isBranchJmp 为真，说明下一周期不需要取指令（发出NOP）, 等待ALU计算redirect结果，再更新
-  pc_reg := Mux(bpu.io.isBranchJmp, Mux(io.redirect.valid, io.redirect.target, pc_reg), pc_reg + 4.U)
+  // 在下一级准备好之前，pc_reg 不变
+  pc_reg := Mux(!io.out.ready, pc_reg, 
+    Mux(!bpu.io.isBranchJmp, pc_reg + 4.U, 
+    Mux(!io.redirect.valid, pc_reg, io.redirect.target))) // 如果是跳转指令，在redirect 为 valid之前，保持pc_reg不变
 
 // imem(SimpleBusUC) ------------------------------------ req(SimpleBusReqBundle)
   // val addr = Output(UInt(PAddrBits.W)) // 访存地址（位宽与体系结构实现相关）, 默认 32 位
@@ -52,16 +55,25 @@ class IFU extends CyhCoreModule with HasResetVector {
 
   io.out       := DontCare
   // 如果发现 isNOP = true.B，说明这一回合应该发送气泡指令，而非真正的指令，真正的指令需要保留一回合
-  io.out.bits.instr := Mux(isNOP, Instructions.NOP, io.imem.resp.rdata)(INST_LEN-1, 0)
+  // io.out.bits.instr := Mux(isNOP, Instructions.NOP, io.imem.resp.rdata)(INST_LEN-1, 0)
+  io.out.bits.instr := (io.imem.resp.rdata)(INST_LEN-1, 0)
   io.out.bits.pc    := pc_reg
 
 // handshake ------------------------------------------------
+  // 一个前提：所有时序电路，最早也是在下个时钟上升沿才会读取我们的输出
+  // 因此，输出只要能在下个时钟上升沿之前准备好，就可以在下个时钟上升沿之前拉高valid（哪怕早于“数据真的准备好了”）
+
+  // 目前，我们的指令内存读取可以在当时周期内完成，且输入并没有valid，输入不关心
+  // 输出部分，当输出的ready不为真，我们的pc不能改变
 
   val rst = Wire(Bool())
   rst := reset
-  io.out.valid := !rst // 目前，IFU只要不是reset，它的输出结果就是有效的
 
-// --- Jump wire of inst to ALU, for calculating next_pc in time ---
+  // 在果壳中，当读取到的指令（内存）为 true时，valid 为 true
+  // 我们这里读取到的指令随时为 true，所以只需要考虑 reset
+  io.out.valid := !rst
+
+// Debug info --------------------------------------------------
 
   Debug("In IFU, The inst read is 0x%x", io.imem.resp.rdata)
 
