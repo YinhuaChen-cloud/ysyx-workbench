009db90a007b22ce7f58567874880a17dd7349fe
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 23:10:16 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:10:16 up 6 days,  4:22,  8 users,  load average: 1.66, 0.87, 0.41

--- npc/playground/src/cyhcore/Hazard.scala
+++ npc/playground/src/cyhcore/Hazard.scala
@@ -35,7 +35,7 @@ class Hazard extends CyhCoreModule {
   rst := reset
   IDUregHalt := RAWhazard
   ISUregHalt := RAWhazard
-  EXUregHalt := RAWhazard
+  EXUregHalt := Mux(RAWhazard, false.B, ISUregHalt)
 
 }
 
