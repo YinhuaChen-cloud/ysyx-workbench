ce48677dd0f2ae7d513aa9e5a50d29a99f72cdca
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 11:05:41 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:05:41 up 5 days, 17:26,  8 users,  load average: 0.57, 0.60, 0.47

--- npc/playground/src/cyhcore/backend/seq/Backend.scala
+++ npc/playground/src/cyhcore/backend/seq/Backend.scala
@@ -43,7 +43,7 @@ class Backend extends CyhCoreModule {
   io.dmem <> exu.io.dmem
 
   // 临时valid
-  wbu.io.valid := io.in.valid
+  wbu.io.valid := exu.io.in.valid
   
   Debug(p"In Backend data, ${io.in.bits.data}")
   Debug(p"In Backend ctrl, ${io.in.bits.ctrl}")
