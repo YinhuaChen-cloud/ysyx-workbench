07a321a98ba6c36ca26800d65cbd9f14bde67cd8
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 18:54:57 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:54:57 up 6 days, 12:46,  8 users,  load average: 0.20, 0.29, 0.23

--- npc/playground/src/cyhcore/Hazard.scala
+++ npc/playground/src/cyhcore/Hazard.scala
@@ -14,11 +14,41 @@ class Hazard extends CyhCoreModule {
   // 由 IFU 自己使用预译码判断是否有控制冒险
   val CtrlHazard = WireInit(false.B)
   BoringUtils.addSink(CtrlHazard, "CtrlHazard")
+  val CtrlHazard_next_cycle = RegNext(CtrlHazard) // 控制冒险的延时一周期
 
   // 由 ISU-ScoreBoard 检测是否有 RAW 冒险
   val RAWhazard = WireInit(false.B)
   BoringUtils.addSink(RAWhazard, "RAWhazard")
-  dontTouch(RAWhazard)
+
+  // ------------------------- 处理控制冒险和数据冒险同时出现的情况 -------------- start
+
+  // 1. 先出现控制冒险，再出现数据冒险: IFU 的指令已经发射，正常等待数据冒险处理，接着等待跳转指令处理即可 -- skip
+  // 2. 先出现数据冒险，再出现控制冒险: WBU 是最后一级，这种情况不存在的 -- skip
+
+  // 3. 两种冒险同时出现：
+  val conflict = RegInit(false.B) // 检测两种冒险是否同时出现
+  when(conflict && !RAWhazard) { // 当RAW冒险已经结束时
+    IDUregControl := true.B
+    conflict := false.B
+  } .elsewhen(!conflict && CtrlHazard && !CtrlHazard_next_cycle && RAWhazard) { // 当出现冒险冲突时
+    IDUregControl := false.B
+    conflict := true.B
+  } .otherwise { // 在没有冲突时
+    IDUregControl := Mux(Flush, false.B,  // flush 时，置 invalid
+      Mux(CtrlHazard, false.B,             // 遇到控制冒险，阻塞IFU->IDU，直到pc_reg跳转（已经延迟一周期）
+      Mux(RAWhazard, false.B,             // RAWhazard 没消失时，置invalid，停住这一级，防止冲刷下级指令
+      true.B)))                           // 平时设置 true.B 即可
+  }
+  // conflict := Mux(!conflict, Mux(CtrlHazard && !CtrlHazard_next_cycle && RAWhazard, true.B, conflict), 
+  //   Mux(!RAWhazard, false.B, conflict))
+
+  // // IDUregControl := Mux(RAWhazard, false.B, !rst && !CtrlHazard)
+  // IDUregControl := Mux(Flush, false.B,  // flush 时，置 invalid
+  //   Mux(CtrlHazard, false.B,             // 遇到控制冒险，阻塞IFU->IDU，直到pc_reg跳转（已经延迟一周期）
+  //   Mux(RAWhazard, false.B,             // RAWhazard 没消失时，置invalid，停住这一级，防止冲刷下级指令
+  //   true.B)))                           // 平时设置 true.B 即可
+
+  // ------------------------- 处理控制冒险和数据冒险同时出现的情况 -------------- end
 
   // IDUreg控制信号
   val IDUregControl = Wire(Bool())
@@ -53,11 +83,8 @@ class Hazard extends CyhCoreModule {
   // 遇到数据冒险时，阻塞整个流水线一个周期
   // 在 RAWhazard后，部分流水线寄存器需要 valid=true.B 一个周期，因此使用下面这个延时一个周期的信号
   val RAWhazard_next_cycle = RegNext(RAWhazard)
-  // IDUregControl := Mux(RAWhazard, false.B, !rst && !CtrlHazard)
-  IDUregControl := Mux(Flush, false.B,  // flush 时，置 invalid
-    Mux(CtrlHazard, false.B,             // 遇到控制冒险，阻塞IFU->IDU，直到pc_reg跳转（已经延迟一周期）
-    Mux(RAWhazard, false.B,             // RAWhazard 没消失时，置invalid，停住这一级，防止冲刷下级指令
-    true.B)))                           // 平时设置 true.B 即可
+
+  // IDUregControl 写在上面
 
   // ISUregControl := Mux(RAWhazard, false.B, IDUregValid)
   ISUregControl := Mux(Flush, false.B,  // flush 时，置 invalid
