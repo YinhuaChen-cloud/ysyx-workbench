43037e7d2783dc3ed508fe034d539f15d3b94ad5
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 17:19:11 +0800

    > generate verilog ysyx_22050039 陈胤桦 Linux chenyinhua-VirtualBox 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:19:11 up 6 days, 11:10,  8 users,  load average: 0.22, 0.34, 0.30

--- npc/playground/src/cyhcore/frontend/IFU.scala
+++ npc/playground/src/cyhcore/frontend/IFU.scala
@@ -40,7 +40,7 @@ class IFU extends CyhCoreModule with HasResetVector {
   // 如果是jmp，也是阻塞流水线(pc不变，下一周期发送NOP)，...... TODO：无条件跳转指令应该能进一步优化
   // 如果不是，就 PC + 4
   // 如果这一周期 bpu.io.isBranchJmp 为真，说明下一周期不需要取指令（发出NOP）, 等待ALU计算redirect结果，再更新
-  pc_reg := Mux(io.out.ready, pc_reg,  // IDUreg 没有准备好接收当前指令，停住 pc_reg 不让其跳转
+  pc_reg := Mux(!io.out.ready, pc_reg,  // IDUreg 没有准备好接收当前指令，停住 pc_reg 不让其跳转
     Mux(!bpu.io.isBranchJmp, pc_reg + 4.U, 
     Mux(!io.redirect.valid, pc_reg, 
     io.redirect.target)))
